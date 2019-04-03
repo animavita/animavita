2989a238de44aa2ca75df72aa011c843274d3c44
Wendel Freitas <wendelfb@bol.com.br>
2019-04-03 16:48:04 -0300

    createToken function to authenticate users in application :star:

--- src/graphql/resolvers/Auth/mutations.js
+++ src/graphql/resolvers/Auth/mutations.js
@@ -1,17 +1,20 @@
 import bcrypt from 'bcrypt';
 import User from '../../../models/User/user.model';
+import { generateToken } from '../../../utils/auth';
 
 const createToken = async (parent, { email, password }, context) => {
   const user = await User.findOne({ email }).select('+password');
 
-  if (!user) {
-    throw new Error('Unauthorized, wrong email or password!');
-  }
-  if (!(await bcrypt.compare(password, user.password))) {
+  if (!user || !(await bcrypt.compare(password, user.password))) {
     throw new Error('Unauthorized, wrong email or password!');
   }
 
-  return user;
+  const token = generateToken({ id: user.id });
+
+  return {
+    user,
+    token
+  };
 };
 
 export { createToken };
