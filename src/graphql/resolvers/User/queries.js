e74a11fcec386bf7952759366dd908794db8ea23
Wendel Freitas <wendelfb@bol.com.br>
2019-04-04 10:59:30 -0300

    Removing unused functions (mongoose) to return users

--- src/graphql/resolvers/User/queries.js
+++ src/graphql/resolvers/User/queries.js
@@ -1,9 +1,7 @@
 import User from '../../../models/User/user.model';
 
 const users = async (parent, args, context, info) => {
-  const user = await User.find({})
-    .populate()
-    .exec();
+  const user = await User.find({});
   return user;
 };
 
