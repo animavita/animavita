d9cf354dcd1f4a07a2cadd95bdd27677d7d8f298
Wendel Freitas <wendelfb@bol.com.br>
2019-04-05 12:52:48 -0300

    Create user resolver with input type

--- src/graphql/resolvers/User/mutations.js
+++ src/graphql/resolvers/User/mutations.js
@@ -1,1 +1,10 @@
-export {};
+import User from '../../../models/User/user.model';
+
+export const createUser = async (parent, { input }, context, info) => {
+  try {
+    const user = await User.create(input);
+    return user;
+  } catch (err) {
+    throw new Error(err);
+  }
+};
