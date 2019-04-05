f5340307721d696bfa891829ff57dc7c587d4f8b
Wendel Freitas <wendelfb@bol.com.br>
2019-04-05 12:56:37 -0300

    Removing export in final file

--- src/graphql/resolvers/User/queries.js
+++ src/graphql/resolvers/User/queries.js
@@ -1,8 +1,6 @@
 import User from '../../../models/User/user.model';
 
-const users = async (parent, args, context, info) => {
+export const users = async (parent, args, context, info) => {
   const user = await User.find({});
   return user;
 };
-
-export { users };
