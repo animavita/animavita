2758cbcd24799c6ff5dfab15539b545cbb141725
Wendel Freitas <wendelfb@bol.com.br>
2019-04-10 12:32:03 -0300

    createUser returning the user created and token generated

--- src/graphql/resolvers/User/mutations.js
+++ src/graphql/resolvers/User/mutations.js
@@ -1,9 +1,11 @@
-import User from '../../../models/User/user.model';
+import User from '~/models/User/user.model';
+import { generateToken } from '~/utils/auth';
 
-export const createUser = async (parent, { input }, context, info) => {
+export const createUser = async (parent, { input }) => {
   try {
     const user = await User.create(input);
-    return user;
+    const token = generateToken({ id: user.id });
+    return { user, token };
   } catch (err) {
     throw new Error(err);
   }
