3d7b17bb3d77a5ebdf5f299d538840f7102a2394
Wendel Freitas <wendelfb@bol.com.br>
2019-04-13 00:36:34 -0300

    changing const name to better code reading

--- src/graphql/resolvers/User/queries.js
+++ src/graphql/resolvers/User/queries.js
@@ -1,6 +1,6 @@
 import User from '../../../models/User/user.model';
 
 export const users = async (parent, args, context, info) => {
-  const user = await User.find({});
-  return user;
+  const allUsers = await User.find({});
+  return allUsers;
 };
