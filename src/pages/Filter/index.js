1f4133452775987ec0325b3e7f77b439d2b61421
Wendel Freitas <wendelfb@bol.com.br>
2019-05-29 21:04:20 -0300

    Adding filter screen to adopt list

---
+++ src/pages/Filter/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { View } from 'react-native';
+import Profile from '~/components/Profile';
+
+import { Container } from './styles';
+
+const Filter = () => (
+  <Container>
+    <Profile title="Filtro" />
+  </Container>
+);
+
+export default Filter;
---
+++ src/pages/Filter/styles.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  padding: 0 30px;
+`;
