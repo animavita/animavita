4f710766027c3a29421d024eab034056698d1b83
Wendel Freitas <wendelfb@bol.com.br>
2019-06-02 20:34:50 -0300

    feat(Login): remodeling login screen

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,77 +1,23 @@
-import React from "react";
-import { Image, View, Text } from "react-native";
-import Swiper from "react-native-swiper";
-import {
-  styles,
-  Container,
-  Background,
-  Footer,
-  Title,
-  About,
-  Slide,
-  Description
-} from "./styles";
-import { Button } from "react-native-elements";
+import React from 'react';
 
-const slides = [
-  {
-    title: "Dogs!",
-    description: `displays a centered icon (when no title) or to the left (with text). (can be used along with iconRight as well). Can be an object or a custom component.`,
-    icon: require("../../images/bone.png")
-  },
-  {
-    title: "Hello Gentleman",
-    description: `displays a centered icon (when no title) or to the left (with text). (can be used along with iconRight as well). Can be an object or a custom component.`,
-    icon: require("../../images/respect.png")
-  },
-  {
-    title: "Winner!",
-    description: `displays a centered icon (when no title) or to the left (with text). (can be used along with iconRight as well). Can be an object or a custom component.`,
-    icon: require("../../images/siren.png")
-  }
-];
+import { View } from 'react-native';
+import { H1, Title } from '~/components';
+import GradientButton from '~/components/GradientButton';
+import { Container } from './styles';
+import { THEME_COLORS } from '~/utils/constants';
 
-handleLoginWithFacebook = () => {
-  alert("Login");
-};
-const Login = props => (
+const Login = () => (
   <Container>
-    <Background>
-      <View style={styles.header}>
-        <Swiper
-          activeDotColor="white"
-          autoplay
-          autoplayTimeout={4}
-          dotStyle={styles.dot}
-          activeDotStyle={styles.dot}
-        >
-          {slides.map((slide, index) => (
-            <Slide key={index}>
-              <Image style={styles.icon} source={slide.icon} />
-              <Title>{slide.title}</Title>
-              <Description>{slide.description}</Description>
-            </Slide>
-          ))}
-        </Swiper>
-      </View>
-    </Background>
-    <Footer>
-      <Button
-        title="ENTRAR COM FACEBOOK"
-        buttonStyle={styles.button}
-        titleStyle={styles.title}
-        onPress={() => props.navigation.navigate("Introduction")}
-      />
-      <About>
-        <Text style={styles.about}>Quer saber mais sobre Animavita?</Text>
-        <Text style={styles.click}>Clique aqui!</Text>
-      </About>
-    </Footer>
+    <H1>Your home.</H1>
+    <Title size={26} color={THEME_COLORS.SECONDARY}>
+      Greener
+    </Title>
+    <GradientButton onPress={() => setFinishStep(false)}>
+      <Title size={14} color="white">
+        Login
+      </Title>
+    </GradientButton>
   </Container>
 );
-
-Login.navigationOptions = {
-  header: null
-};
 
 export default Login;
