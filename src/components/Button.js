e16c46d9321266b9906aca2d988292c94110f702
Wendel Freitas <wendelfb@bol.com.br>
2019-06-02 20:30:31 -0300

    Button with active prop to add linear gradient style

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components/native';
 import PropTypes from 'prop-types';
 import { THEME_COLORS } from '~/utils/constants';
+import LinearGradient from 'react-native-linear-gradient';
 
 const Container = styled.TouchableOpacity`
   border-width: 1.5px;
@@ -13,28 +14,69 @@ const Container = styled.TouchableOpacity`
 `;
 
 const Title = styled.Text`
-  color: ${THEME_COLORS.SECONDARY};
+  color: ${props => (props.color ? props.color : THEME_COLORS.SECONDARY)};
   font-size: 12;
+  font-weight: ${props => (props.weight ? props.weight : 'normal')};
 `;
 
+const start = {
+  x: 0,
+  y: 0,
+};
+
+const end = {
+  x: 1,
+  y: 1,
+};
+
+const ButtonGradient = styled(LinearGradient)`
+  justify-content: center;
+  align-items: center;
+  padding: 8px 25px;
+  border-radius: 20px;
+`;
+
+const GradientContainer = styled.TouchableOpacity`
+  justify-content: center;
+  align-items: center;
+  border-radius: 20px;
+`;
+
+const locations = [0.1, 0.9];
+
 const Button = ({
-  title, fontColor, containerColor, onPress,
-}) => (
-  <Container color={containerColor} onPress={onPress}>
+  title, fontColor, containerColor, onPress, active,
+}) => (active ? (
+  <GradientContainer color={containerColor} activeOpacity={0.5} onPress={onPress}>
+    <ButtonGradient
+      start={start}
+      end={end}
+      locations={locations}
+      colors={[THEME_COLORS.PRIMARY, THEME_COLORS.SECONDARY]}
+    >
+      <Title color="white" weight="500">
+        {title}
+      </Title>
+    </ButtonGradient>
+  </GradientContainer>
+) : (
+  <Container color={containerColor} activeOpacity={0.5} onPress={onPress}>
     <Title color={fontColor}>{title}</Title>
   </Container>
-);
+));
 
 Button.propTypes = {
   title: PropTypes.string.isRequired,
   fontColor: PropTypes.string,
   containerColor: PropTypes.string,
   onPress: PropTypes.func.isRequired,
+  active: PropTypes.bool,
 };
 
 Button.defaultProps = {
   fontColor: THEME_COLORS.SECONDARY,
   containerColor: THEME_COLORS.SECONDARY,
+  active: false,
 };
 
 export default Button;
