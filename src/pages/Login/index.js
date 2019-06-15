128e505f3ce1e5a188c87a6c49b239dda620f032
Wendel Freitas <wendelfb@bol.com.br>
2019-06-15 20:36:17 -0300

    Loading props

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,23 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AsyncStorage from '@react-native-community/async-storage';
 import { H1, Title } from '~/components';
 import GradientButton from '~/components/GradientButton';
-import { Container } from './styles';
+import PropTypes from 'prop-types';
 import { THEME_COLORS } from '~/utils/constants';
-
 import { handleLoginFacebook } from '~/services/authFacebook';
+import { Container } from './styles';
 
 const Login = ({ navigation }) => {
+  const [loading, useLoading] = useState(false);
+
   async function handleLogin() {
-    const response = await handleLoginFacebook();
+    useLoading(true);
 
-    if (response.error) {
-      console.log(response.error);
-    }
+    const user = JSON.parse(await AsyncStorage.getItem('@animavita:facebook_user'));
 
-    if (response.user) {
-      await AsyncStorage.setItem('@animativa:facebook_user', JSON.stringify(response.user));
+    if (user) {
       navigation.navigate('Home');
+    } else {
+      const response = await handleLoginFacebook();
+
+      if (response.error) {
+        console.log(response.error);
+      }
+
+      if (response.user) {
+        await AsyncStorage.setItem('@animativa:facebook_user', JSON.stringify(response.user));
+        navigation.navigate('Home');
+      }
     }
   }
 
@@ -27,13 +37,19 @@ const Login = ({ navigation }) => {
       <Title size={26} color={THEME_COLORS.SECONDARY}>
         Greener
       </Title>
-      <GradientButton onPress={() => handleLogin()}>
+      <GradientButton onPress={() => handleLogin()} loading={loading}>
         <Title size={12} color="white">
           Entrar com facebook
         </Title>
       </GradientButton>
     </Container>
   );
+};
+
+Login.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func.isRequired,
+  }).isRequired,
 };
 
 export default Login;
