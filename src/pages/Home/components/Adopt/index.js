2171d89250810bb0913e350e3cb0a190b0559b0a
Wendel Freitas <wendelfb@bol.com.br>
2019-08-18 19:59:04 -0300

    trait query errors and fetchMore adoptions

--- src/pages/Home/components/Adopt/index.js
+++ src/pages/Home/components/Adopt/index.js
@@ -1,4 +1,6 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { showMessage } from 'react-native-flash-message';
 import { isEmpty } from '~/utils/helpers';
 import { gql } from 'apollo-boost';
 import Button from '~/components/Button';
@@ -8,6 +10,7 @@ import Swiper from 'react-native-deck-swiper';
 import Pet from './Pet';
 import Loading from '~/components/Loading';
 import { Container, TopButtons } from './styles';
+
 
 const GET_ADOPTS_QUERY = gql`
   query getAllAdopts($filter: AdoptsFilter, $skip: Int, $first: Int) {
@@ -23,58 +26,64 @@ const GET_ADOPTS_QUERY = gql`
 
 const Adopt = ({ navigation }) => {
   const filters = useSelector(state => state.filter);
-  let adoptsData = [];
-  const { loading, error, data } = useQuery(GET_ADOPTS_QUERY, {
+  const {
+    loading, error, data, fetchMore,
+  } = useQuery(GET_ADOPTS_QUERY, {
     variables: {
       filter: filters,
       skip: 0,
-      first: 10,
+      first: 2,
     },
   });
 
-  if (!loading) {
-    console.log(data);
-    console.log(loading);
-    console.log(error);
-    adoptsData = data.adopts;
-  }
+  useEffect(() => {
+    if (error) {
+      showMessage({
+        message: 'Erro na listagem de adoções!',
+        description:
+          'Ops! Alguns animais escaparam dos nossos abraços, tente novamente mais tarde!',
+        type: 'danger',
+      });
+    }
+  }, [error]);
 
   function handleRedirectToDetail(animal) {
     navigation.navigate('Details', { animal });
   }
 
-  function nextAnimal(index) {
-    // if(data.allAdopts[index + 1]){
-    //   alert('exist')
-    //   fetchMore({
-    //     variables: {
-    //       quantity: 2
-    //     },
-    //     updateQuery: (previousResult, { fetchMoreResult }) => {
-    //       if (!fetchMoreResult) {
-    //         return previousResult;
-    //       }
-    //       alert(JSON.stringify(fetchMoreResult))
-    //       alert(JSON.stringify(previousResult))
-    //       return {
-    //         ...fetchMoreResult,
-    //       };
-    //     },
-    //   });
-    // }
+  function nextAdoption(index) {
+    if (data.adopts[index + 1]) {
+      fetchMore({
+        variables: {
+          skip: data.adopts.length,
+        },
+        updateQuery: (prev, { fetchMoreResult }) => {
+          if (!fetchMoreResult) return prev;
+          return Object.assign({}, prev, {
+            adopts: [...prev.adopts, ...fetchMoreResult.adopts],
+          });
+        },
+      });
+    }
   }
 
+  if(data.adopts){
+
+    alert(JSON.stringify(data.adopts))
+  }
+
+
   function renderSwiper() {
-    return isEmpty(adoptsData) ? null : (
+    return isEmpty(data.adopts) ? null : (
       <Swiper
-        cards={adoptsData}
+        cards={data.adopts || []}
         cardVerticalMargin={0}
         useViewOverflow={false}
         verticalSwipe={false}
-        onTapCard={index => handleRedirectToDetail(data.allAdopts[index])}
+        onTapCard={index => handleRedirectToDetail(data.adopts[index])}
         // eslint-disable-next-line no-underscore-dangle
         renderCard={animal => <Pet key={animal._id} animal={animal} />}
-        onSwiped={cardIndex => nextAnimal(cardIndex)}
+        onSwiped={cardIndex => nextAdoption(cardIndex)}
         cardIndex={0}
         backgroundColor="#ffffff"
         stackSize={3}
@@ -91,6 +100,12 @@ const Adopt = ({ navigation }) => {
       <Container>{loading ? <Loading /> : renderSwiper()}</Container>
     </Fragment>
   );
+};
+
+Adopt.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func.isRequired,
+  }).isRequired,
 };
 
 export default Adopt;
