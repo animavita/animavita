4dd09519aa002ead6595d4e2f157523bbe4d6020
Wendel Freitas <wendelfb@bol.com.br>
2019-08-22 17:04:40 -0300

    Image style for error component

--- src/pages/Home/components/Adopt/styles.js
+++ src/pages/Home/components/Adopt/styles.js
@@ -9,15 +9,17 @@ export const TopButtons = styled.View`
   flex-direction: row;
   justify-content: space-between;
   padding: 0 30px;
-  margin-bottom: 10px;
 `;
 
 export const ImageContainer = styled.View`
   flex: 1;
-  justify-content: center;
   align-items: center;
 `;
 
 export const styles = StyleSheet.create({
-  image: { width: '100%', height: '80%', marginTop: -15 },
+  image: {
+    width: '80%',
+    height: '80%',
+    marginBottom: -35,
+  },
 });
