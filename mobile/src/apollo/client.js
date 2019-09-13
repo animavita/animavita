61cda56d3b61bdd97092c2b1ede1cc65f8634e5f
Wendel Freitas <wendelfreitasbranco@gmail.com>
2019-09-13 16:30:08 -0300

    Remove unnecessary comments from apollo client

--- mobile/src/apollo/client.js
+++ mobile/src/apollo/client.js
@@ -10,9 +10,7 @@ const httpLink = new HttpLink({
 });
 
 const authLink = setContext(async (_, { headers }) => {
-  // get the authentication token from local storage if it exists
   const { token } = await getUser();
-  // return the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
