3ddacb0a521f7ac33635af4a0c758fc484d5fe08
Wendel Freitas <wendelfreitasbranco@gmail.com>
2019-09-15 03:05:31 -0300

    changeSetting mutation to user configurations

--- mobile/src/pages/Settings/components/Notification/index.js
+++ mobile/src/pages/Settings/components/Notification/index.js
@@ -1,34 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Switch } from 'react-native';
 import { THEME_COLORS } from '~/utils/constants';
-
+import { useMutation } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
 import { FormContainer, Input, Field } from '~/components';
+import PropTypes from 'prop-types';
 
-const Notification = () => (
-  <FormContainer>
-    <Input>
-      <Field>Notificações</Field>
-      <Switch
-        value
-        thumbColor={THEME_COLORS.SECONDARY}
-        trackColor={{
-          true: THEME_COLORS.SECONDARY,
-        }}
-        onValueChange={value => console.log(value)}
-      />
-    </Input>
-    <Input>
-      <Field>Eu sou um herói</Field>
-      <Switch
-        value
-        thumbColor={THEME_COLORS.SECONDARY}
-        trackColor={{
-          true: THEME_COLORS.SECONDARY,
-        }}
-        onValueChange={value => console.log(value)}
-      />
-    </Input>
-  </FormContainer>
-);
+const USER_CHANGE_SETTING_MUTATION = gql`
+  mutation ChangeSettingMutation($option: String!) {
+    ChangeSettingMutation(input: { option: $option }) {
+      user {
+        hero
+        notifications
+      }
+    }
+  }
+`;
+
+const Notification = ({ user }) => {
+  const [settings, setSettings] = useState({
+    notifications: user.notifications,
+    hero: user.hero,
+  });
+  const [changeSetting] = useMutation(USER_CHANGE_SETTING_MUTATION, {
+    onCompleted: ({ ChangeSettingMutation }) => {
+      setSettings(ChangeSettingMutation.user);
+    },
+  });
+
+  return (
+    <FormContainer>
+      <Input>
+        <Field>Notificações</Field>
+        <Switch
+          value={settings.notifications}
+          thumbColor={THEME_COLORS.SECONDARY}
+          trackColor={{
+            true: THEME_COLORS.SECONDARY,
+          }}
+          onValueChange={() => changeSetting({ variables: { option: 'notifications' } })}
+        />
+      </Input>
+      <Input>
+        <Field>Eu sou um herói</Field>
+        <Switch
+          value={settings.hero}
+          thumbColor={THEME_COLORS.SECONDARY}
+          trackColor={{
+            true: THEME_COLORS.SECONDARY,
+          }}
+          onValueChange={() => changeSetting({ variables: { option: 'hero' } })}
+        />
+      </Input>
+    </FormContainer>
+  );
+};
+
+Notification.propTypes = {
+  user: PropTypes.shape({
+    hero: PropTypes.bool,
+    notifications: PropTypes.bool,
+  }).isRequired,
+};
 
 export default Notification;
