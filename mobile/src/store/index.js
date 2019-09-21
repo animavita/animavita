75c609fa66da2a05b401f10769accddaa9ac2545
Wendel Freitas <wendelfreitasbranco@gmail.com>
2019-09-21 17:24:17 -0300

    redux-persist initial config

--- mobile/src/store/index.js
+++ mobile/src/store/index.js
@@ -1,9 +1,16 @@
+import AsyncStorage from '@react-native-community/async-storage';
 import { createStore, compose, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
 import createSagaMiddleware from 'redux-saga';
-
 import reducers from './ducks';
 import sagas from './sagas';
 
+const persistConfig = {
+  key: 'root',
+  timeout: 0,
+  storage: AsyncStorage,
+  whitelist: ['auth'],
+};
 const middlewares = [];
 
 const sagaMonitor = __DEV__ ? console.tron.createSagaMonitor() : null;
@@ -19,8 +26,11 @@ const composer = __DEV__
   )
   : compose(applyMiddleware(...middlewares));
 
-const store = createStore(reducers, composer);
+const persistedReducer = persistReducer(persistConfig, reducers);
+const store = createStore(persistedReducer, composer);
+
+const persistor = persistStore(store);
 
 sagaMiddleware.run(sagas);
 
-export default store;
+export { store, persistor };
