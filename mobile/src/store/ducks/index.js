f045a412c8208876725b7430761749ec23ed4d98
Wendel Freitas <wendelfreitasbranco@gmail.com>
2019-09-21 17:24:01 -0300

    combine auth reducer

--- mobile/src/store/ducks/index.js
+++ mobile/src/store/ducks/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import filter from './filter';
+import auth from './auth';
 
 const reducers = combineReducers({
   filter,
+  auth,
 });
 
 export default reducers;
