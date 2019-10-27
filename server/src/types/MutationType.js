fa766996dabe9e9f8fefe9cccab6877b57898cda
Wendel Freitas <wendelfreitasbranco@gmail.com>
2019-10-27 17:41:13 -0300

    adding MessageMutations in mutations

--- server/src/types/MutationType.js
+++ server/src/types/MutationType.js
@@ -2,11 +2,13 @@ import { GraphQLObjectType } from 'graphql';
 
 import UserMutations from '../modules/user/mutation';
 import AdoptMutations from '../modules/adopt/mutation';
+import MessageMutations from '../modules/message/mutation';
 
 export default new GraphQLObjectType({
   name: 'Mutation',
   fields: () => ({
     ...UserMutations,
-    ...AdoptMutations
+    ...AdoptMutations,
+    ...MessageMutations
   })
 });
