cca540693f114080f6dc23ba873e175c4a243e4a
Wendel Freitas <wendelfreitasbranco@gmail.com>
2019-10-28 15:43:16 -0300

    message subscription with filter

--- server/src/modules/message/subscription/MessageSendedSubscription.js
+++ server/src/modules/message/subscription/MessageSendedSubscription.js
@@ -1,9 +1,20 @@
+/* eslint-disable max-len */
+import { withFilter } from 'graphql-subscriptions';
+import { GraphQLNonNull, GraphQLString } from 'graphql';
 import MessageType from '../MessageType';
 import pubSub, { EVENTS } from '~/pubSub';
 
 const MessageSendedSubscription = {
   type: MessageType,
-  subscribe: () => pubSub.asyncIterator(EVENTS.MESSAGE.SENDED)
+  args: {
+    conversationId: {
+      type: new GraphQLNonNull(GraphQLString)
+    }
+  },
+  subscribe: withFilter(
+    () => pubSub.asyncIterator(EVENTS.MESSAGE.SENDED),
+    (payload, args) => payload.MessageSended.conversation.toString() === args.conversationId.toString()
+  )
 };
 
 export default MessageSendedSubscription;
