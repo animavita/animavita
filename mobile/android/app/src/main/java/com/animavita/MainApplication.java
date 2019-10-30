cc96cf54b5ac38ffd2bd86b053c9c01dfff17cdc
Wendel Freitas <wendelfreitasbranco@gmail.com>
2019-10-30 03:34:51 -0300

    OneSignal configs in react native

--- mobile/android/app/src/main/java/com/animavita/MainApplication.java
+++ mobile/android/app/src/main/java/com/animavita/MainApplication.java
@@ -3,6 +3,7 @@ package com.animavita;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.geektime.rnonesignalandroid.ReactNativeOneSignalPackage;
 import com.imagepicker.ImagePickerPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
@@ -41,6 +42,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeOneSignalPackage(),
             new ImagePickerPackage(),
             new AsyncStoragePackage(),
             new FBSDKPackage(mCallbackManager),
