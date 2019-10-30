9b84281aed082a6b7982f4cf4a848861328d062e
Wendel Freitas <wendelfreitasbranco@gmail.com>
2019-10-30 03:35:06 -0300

    initializing OneSignal in app

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import OneSignal from 'react-native-onesignal';
+import { ONE_SIGNAL_APP_KEY } from '~/utils/constants';
 import FlashMessage from 'react-native-flash-message';
 import { ApolloProvider } from '@apollo/react-hooks';
 import { Provider } from 'react-redux';
@@ -14,6 +16,21 @@ const App = () => {
   const [signed, setSigned] = useState(false);
   const [loading, setLoading] = useState(true);
 
+  function onReceived(notification) {
+    console.log('Notification received: ', notification);
+  }
+
+  function onOpened(openResult) {
+    console.log('Message: ', openResult.notification.payload.body);
+    console.log('Data: ', openResult.notification.payload.additionalData);
+    console.log('isActive: ', openResult.notification.isAppInFocus);
+    console.log('openResult: ', openResult);
+  }
+
+  function onIds(device) {
+    console.log('Device info: ', device);
+  }
+
   async function getData() {
     const token = await getToken();
     if (token) {
@@ -24,6 +41,15 @@ const App = () => {
 
   useEffect(() => {
     getData();
+    OneSignal.init(ONE_SIGNAL_APP_KEY);
+    OneSignal.addEventListener('received', onReceived);
+    OneSignal.addEventListener('opened', onOpened);
+    OneSignal.addEventListener('ids', onIds);
+    return () => {
+      OneSignal.removeEventListener('received', this.onReceived);
+      OneSignal.removeEventListener('opened', this.onOpened);
+      OneSignal.removeEventListener('ids', this.onIds);
+    };
   }, []);
 
   if (loading) {
