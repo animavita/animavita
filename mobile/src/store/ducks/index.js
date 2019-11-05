63bddcb6d7c3a2c2eeda7df3c1096386a1a3f570
Wendel Freitas <wendelfreitasbranco@gmail.com>
2019-11-05 06:09:08 -0200

    create favorite reducer

--- mobile/src/store/ducks/index.js
+++ mobile/src/store/ducks/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import filter from './filter';
+import favorite from './favorite';
 import auth from './auth';
 
 const reducers = combineReducers({
   filter,
+  favorite,
   auth,
 });
 
