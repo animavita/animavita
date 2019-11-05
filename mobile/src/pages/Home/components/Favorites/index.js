9f778eb86af719999b92668b4acecaf01a581329
Wendel Freitas <wendelfreitasbranco@gmail.com>
2019-11-05 06:11:03 -0200

    remove statistics component

---
+++ mobile/src/pages/Home/components/Favorites/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { View } from 'react-native';
+
+const Favorites = () => <View />;
+
+export default Favorites;
--- mobile/src/pages/Home/components/Statistics/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import { View, Dimensions } from 'react-native';
-
-// import { Container } from './styles';
-
-const Statistics = () => <View style={{ alignItems: 'center' }} />;
-
-export default Statistics;
