9514e07f9c067dd95f37825e2974ce91a9bcec21
Wendel Freitas <wendelfreitasbranco@gmail.com>
2019-11-05 06:09:18 -0200

    adding favorite to persist and remove saga

--- mobile/src/store/index.js
+++ mobile/src/store/index.js
@@ -1,23 +1,15 @@
 import AsyncStorage from '@react-native-community/async-storage';
 import { createStore, compose, applyMiddleware } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
-import createSagaMiddleware from 'redux-saga';
 import reducers from './ducks';
-import sagas from './sagas';
 
 const persistConfig = {
   key: 'root',
   timeout: 0,
   storage: AsyncStorage,
-  whitelist: ['auth'],
+  whitelist: ['auth', 'favorite'],
 };
 const middlewares = [];
-
-const sagaMonitor = __DEV__ ? console.tron.createSagaMonitor() : null;
-
-const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
-
-middlewares.push(sagaMiddleware);
 
 const composer = __DEV__
   ? compose(
@@ -30,7 +22,5 @@ const persistedReducer = persistReducer(persistConfig, reducers);
 const store = createStore(persistedReducer, composer);
 
 const persistor = persistStore(store);
-
-sagaMiddleware.run(sagas);
 
 export { store, persistor };
