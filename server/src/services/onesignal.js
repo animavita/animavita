d382b278cf6eee6c74c563e02ce4f12798ed8da3
Wendel Freitas <wendelfreitasbranco@gmail.com>
2019-11-08 04:34:31 -0200

    icons for push notifications

--- server/src/services/onesignal.js
+++ server/src/services/onesignal.js
@@ -20,7 +20,11 @@ const OneSignal = {
     if (userPushTokens.length > 0) {
       onesignal.post('notifications', {
         ...contents,
-        include_player_ids: userPushTokens
+        include_player_ids: userPushTokens,
+        small_icon:
+          'https://raw.githubusercontent.com/wendelfreitas/animavita/master/mobile/android/app/src/main/res/drawable-xxxhdpi/icon.png?token=AID6CIPQZ5JCZOQKCUJW4XC5ZPXHE',
+        large_icon:
+          'https://raw.githubusercontent.com/wendelfreitas/animavita/master/mobile/android/app/src/main/res/drawable-xxxhdpi/icon.png?token=AID6CIPQZ5JCZOQKCUJW4XC5ZPXHE'
       });
     }
   }
