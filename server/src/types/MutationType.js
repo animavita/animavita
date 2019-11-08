60b5719f41abeb3382825eabd0c5417f33f7bc77
Wendel Freitas <wendelfreitasbranco@gmail.com>
2019-11-08 00:31:06 -0200

    Solicitation Mutations

--- server/src/types/MutationType.js
+++ server/src/types/MutationType.js
@@ -3,12 +3,14 @@ import { GraphQLObjectType } from 'graphql';
 import UserMutations from '../modules/user/mutation';
 import AdoptMutations from '../modules/adopt/mutation';
 import MessageMutations from '../modules/message/mutation';
+import SolicitationMutations from '../modules/solicitation/mutation';
 
 export default new GraphQLObjectType({
   name: 'Mutation',
   fields: () => ({
     ...UserMutations,
     ...AdoptMutations,
-    ...MessageMutations
+    ...MessageMutations,
+    ...SolicitationMutations
   })
 });
