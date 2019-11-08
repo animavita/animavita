ddd8e5606f0a1b9932a36befdf7abf0594fb7221
Wendel Freitas <wendelfreitasbranco@gmail.com>
2019-11-08 00:30:40 -0200

    Solicitation subscriptions

--- server/src/types/SubscriptionType.js
+++ server/src/types/SubscriptionType.js
@@ -1,10 +1,12 @@
 import { GraphQLObjectType } from 'graphql';
 
 import MessageSubscriptions from '../modules/message/subscription';
+import SolicitationSubscriptions from '../modules/solicitation/subscription';
 
 export default new GraphQLObjectType({
   name: 'Subscription',
   fields: {
+    ...SolicitationSubscriptions,
     ...MessageSubscriptions
   }
 });
