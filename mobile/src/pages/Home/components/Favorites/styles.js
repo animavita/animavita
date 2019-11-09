9dd76f69d05c8ecea0b438e10623e542e13a9148
Wendel Freitas <wendelfreitasbranco@gmail.com>
2019-11-09 06:33:15 -0200

    favorite list

---
+++ mobile/src/pages/Home/components/Favorites/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components/native';
+import { StyleSheet } from 'react-native';
+import { THEME_COLORS } from '~/utils/constants';
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: white;
+  justify-content: space-between;
+  padding: 0px 30px;
+`;
+
+export const styles = StyleSheet.create({
+  title: { color: THEME_COLORS.SECONDARY, fontWeight: 'bold' },
+});
