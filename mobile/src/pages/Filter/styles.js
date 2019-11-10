7634e3c7b896912859e09eab635dc40376696fb7
Wendel Freitas <wendelfreitasbranco@gmail.com>
2019-11-10 05:40:18 -0200

    picker style in style file

--- mobile/src/pages/Filter/styles.js
+++ mobile/src/pages/Filter/styles.js
@@ -1,4 +1,6 @@
 import styled from 'styled-components/native';
+import { StyleSheet } from 'react-native';
+import { THEME_COLORS } from '~/utils/constants';
 
 export const Container = styled.View`
   padding: 0 30px;
@@ -15,8 +17,13 @@ export const ButtonGroup = styled.View`
 export const Footer = styled.View`
   align-items: center;
   width: 100%;
+  margin-top: 15px;
 `;
 
 export const ClearText = styled.TouchableOpacity`
   margin-top: 15px;
 `;
+
+export const styles = StyleSheet.create({
+  picker: { height: 50, width: '100%', color: THEME_COLORS.BLACK },
+});
