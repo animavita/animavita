7583391c3c498f1bef6469fe589e00715dbfc618
Wendel Freitas <wendelfreitasbranco@gmail.com>
2019-11-10 05:38:58 -0200

    Notifications page

--- mobile/src/pages/Notifications/index.js
+++ mobile/src/pages/Notifications/index.js
@@ -1,63 +1,120 @@
-import React from 'react';
-import TouchableScale from 'react-native-touchable-scale'; // https://github.com/kohver/react-native-touchable-scale
-import LinearGradient from 'react-native-linear-gradient';
-import { View, Text } from 'react-native';
-import { Avatar, ListItem } from 'react-native-elements';
-
-import { H1 } from '~/components';
-import { Container, Content, Item } from './styles';
+import React, { useState } from 'react';
+import { FlatList, View } from 'react-native';
+import Loading from '~/components/Loading';
+import ListItem from '~/components/ListItem';
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
+import { showMessage } from 'react-native-flash-message';
 import Profile from '~/components/Profile';
+import ErrorContainer from '~/components/ErrorContainer';
+import { Container, Content, styles } from './styles';
+import { isEmpty } from '~/utils/helpers';
 
-const Notifications = () => (
-  <Container>
-    <Profile title="Notificações" />
+const GET_CONVERSATIONS_QUERY = gql`
+  query getAllConversations($skip: Int, $first: Int) {
+    conversations(skip: $skip, first: $first) {
+      _id
+      lastMessage {
+        text
+      }
+      members {
+        _id
+        fullname
+        avatar
+      }
+    }
+  }
+`;
 
-    <Content>
-      <ListItem
-        Component={TouchableScale}
-        containerStyle={{ borderRadius: 15 }}
-        friction={90} //
-        tension={100} // These props are passed to the parent component (here TouchableScale)
-        activeScale={0.95} //
-        linearGradientProps={{
-          colors: ['#0AC4BA', '#2BDA8E'],
-          start: { x: 1, y: 0 },
-          end: { x: 0.2, y: 0 },
-        }}
-        ViewComponent={LinearGradient} // Only if no expo
-        leftAvatar={{
-          rounded: true,
-          source: { uri: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg' },
-        }}
-        title="Chris Jackson"
-        titleStyle={{ color: 'white', fontWeight: '500', fontSize: 14 }}
-        subtitleStyle={{ color: 'white', fontSize: 12 }}
-        subtitle="enviou solicitação para adotar Luke!"
+const Notifications = () => {
+  const [notifications, setNotifications] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [fetchMoreLoading, setfetchMoreLoading] = useState(false);
+
+  const { data, fetchMore } = useQuery(GET_CONVERSATIONS_QUERY, {
+    variables: {
+      skip: 0,
+      first: 10,
+    },
+    fetchPolicy: 'no-cache',
+    onCompleted: (response) => {
+      setNotifications(response.conversations);
+      setLoading(false);
+    },
+    onError: () => {
+      showMessage({
+        message: 'Erro na listagem de conversas!',
+        description: 'Ops! Algum erro aconteceu, tente novamente mais tarde!',
+        type: 'danger',
+      });
+      setLoading(false);
+    },
+  });
+
+  function fetchMoreConversations() {
+    if (notifications.length >= 10) {
+      setfetchMoreLoading(true);
+      fetchMore({
+        variables: {
+          skip: data.conversations.length,
+        },
+        updateQuery: (prev, { fetchMoreResult }) => {
+          setfetchMoreLoading(false);
+          if (!fetchMoreResult) return prev;
+          return setNotifications([...prev.conversations, ...fetchMoreResult.conversations]);
+        },
+      });
+    }
+  }
+
+  function renderFooter() {
+    if (!fetchMoreLoading) return null;
+    return (
+      <View style={styles.loadingContainer}>
+        <Loading size={25} />
+      </View>
+    );
+  }
+
+  function renderContent() {
+    if (isEmpty(notifications)) {
+      return (
+        <ErrorContainer
+          image={require('~/images/emptyNotifications.jpg')}
+          title={'\nSua caixa de notificações está vazia!'}
+          description={
+            '\n Hmmm, acho que não temos nada para te contar... Na verdade temos sim, você sabia que ter um pet reduz a chance de depressão e o stress? \nEles são incríveis e fofos.'
+          }
+        />
+      );
+    }
+
+    return (
+      <FlatList
+        data={notifications}
+        showsVerticalScrollIndicator={false}
+        renderItem={({ item: conversation }) => (
+          <ListItem
+            title="Bruno Gustavo"
+            subtitle="enviou uma solicitação para adotar Luke!"
+            onPress={() => null}
+            avatar={conversation.members[0].avatar}
+          />
+        )}
+        keyExtractor={conversation => conversation._id}
+        onEndReached={fetchMoreConversations}
+        onEndReachedThreshold={0.2}
+        ListFooterComponent={renderFooter}
       />
-      <ListItem
-        Component={TouchableScale}
-        containerStyle={{ borderRadius: 15, marginTop: 15 }}
-        friction={90} //
-        tension={100} // These props are passed to the parent component (here TouchableScale)
-        activeScale={0.95} //
-        linearGradientProps={{
-          colors: ['#0AC4BA', '#2BDA8E'],
-          start: { x: 1, y: 0 },
-          end: { x: 0.2, y: 0 },
-        }}
-        ViewComponent={LinearGradient} // Only if no expo
-        leftAvatar={{
-          rounded: true,
-          source: { uri: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg' },
-        }}
-        title="Chris Jackson"
-        titleStyle={{ color: 'white', fontWeight: '500', fontSize: 14 }}
-        onPress={() => navigation.navigate('Chat')}
-        subtitleStyle={{ color: 'white', fontSize: 12 }}
-        subtitle="enviou solicitação para adotar Luke!"
-      />
-    </Content>
-  </Container>
-);
+    );
+  }
+
+  return (
+    <Container>
+      <Profile title="Notificações" />
+      <Content>{loading ? <Loading /> : renderContent()}</Content>
+    </Container>
+  );
+};
 
 export default Notifications;
--- mobile/src/pages/Notifications/styles.js
+++ mobile/src/pages/Notifications/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components/native';
+import { StyleSheet } from 'react-native';
 
 export const Container = styled.View`
   flex: 1;
@@ -7,4 +8,11 @@ export const Container = styled.View`
 
 export const Content = styled.View`
   margin-top: ${25 * 0.7};
+  flex: 1;
 `;
+
+export const styles = StyleSheet.create({
+  loadingContainer: {
+    marginVertical: 10,
+  },
+});
