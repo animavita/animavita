0795d3cb0efd497aacdd88a1eb249999a9b2931c
Bruno Gustavo <brugucolosil95@gmail.com>
2019-12-01 01:00:42 -0300

    added responsive styles in StyleSheet and padding of container

--- mobile/src/pages/Home/components/Favorites/styles.js
+++ mobile/src/pages/Home/components/Favorites/styles.js
@@ -1,14 +1,19 @@
 import styled from 'styled-components/native';
 import { StyleSheet } from 'react-native';
 import { THEME_COLORS } from '~/utils/constants';
+import { heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
 export const Container = styled.View`
   flex: 1;
   background-color: white;
   justify-content: space-between;
-  padding: 0px 30px;
+  padding: 0px ${`${Math.floor(hp('2%'))}px`};
 `;
 
 export const styles = StyleSheet.create({
-  title: { color: THEME_COLORS.SECONDARY, fontWeight: 'bold' },
+  heart: hp('2.5%'),
+  chevron: hp('5%'),
+  size: hp('8.5%'),
+  subtitle: { fontSize: hp('2.5%') },
+  title: { color: THEME_COLORS.SECONDARY, fontWeight: 'bold', fontSize: hp('3.5%') },
 });
