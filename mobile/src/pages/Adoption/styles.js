de6508ec0dd9e75d2f5b610c047c938c80c6102b
Bruno Gustavo <brugucolosil95@gmail.com>
2019-12-01 00:46:44 -0300

    added responsive padding for container

--- mobile/src/pages/Adoption/styles.js
+++ mobile/src/pages/Adoption/styles.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components/native';
+import { heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
 export const Container = styled.View`
-  padding: 0 30px;
+  padding: 0px ${`${Math.floor(hp('4%'))}px`}
 `;
