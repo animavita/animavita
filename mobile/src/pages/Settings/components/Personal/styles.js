855ab5880b7ea9a3e5bd327e29f74b67563c1b45
Bruno Gustavo <brugucolosil95@gmail.com>
2019-12-01 01:09:25 -0300

    changed value of title and added responsive parameter on font-size

--- mobile/src/pages/Settings/components/Personal/styles.js
+++ mobile/src/pages/Settings/components/Personal/styles.js
@@ -2,7 +2,11 @@ import styled from 'styled-components/native';
 import { TextInput as DefaultInput } from 'react-native';
 import { THEME_COLORS } from '~/utils/constants';
 
+import { heightPercentageToDP as hp } from 'react-native-responsive-screen';
+
+
 export const TextInput = styled(DefaultInput)`
+  font-size: ${hp('2%')};
   padding: 0;
   color: ${THEME_COLORS.BLACK};
 `;
--- mobile/src/pages/Settings/index.js
+++ mobile/src/pages/Settings/index.js
@@ -39,7 +39,7 @@ const Settings = ({ navigation }) => {
         <Configuration user={user} />
         <System>
           <Logout onPress={() => logout()}>
-            <Title color="red" size={14} weight="normal">
+            <Title color="red" size={3} weight="normal">
               SAIR DO APLICATIVO
             </Title>
           </Logout>
