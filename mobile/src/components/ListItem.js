905578f3b19acec5ef3a380b395b2913c46cce8a
Bruno Gustavo <brugucolosil95@gmail.com>
2019-12-01 00:41:10 -0300

    responsive styles in title and size

--- mobile/src/components/ListItem.js
+++ mobile/src/components/ListItem.js
@@ -4,10 +4,13 @@ import { StyleSheet } from 'react-native';
 import TouchableScale from 'react-native-touchable-scale';
 import LinearGradient from 'react-native-linear-gradient';
 import { ListItem as Item } from 'react-native-elements';
+import { heightPercentageToDP as hp } from 'react-native-responsive-screen';
+
 
 const styles = StyleSheet.create({
-  title: { color: 'white', fontWeight: '500', fontSize: 15 },
+  title: { color: 'white', fontWeight: '500', fontSize: hp('2.3%') },
   subtitle: { color: 'white' },
+  size: hp('8%'),
   container: { borderRadius: 15, marginVertical: 5 },
 });
 
@@ -29,6 +32,7 @@ const ListItem = ({
     leftAvatar={{
       rounded: true,
       source: { uri: avatar },
+      size: styles.size,
     }}
     title={title}
     titleStyle={styles.title}
