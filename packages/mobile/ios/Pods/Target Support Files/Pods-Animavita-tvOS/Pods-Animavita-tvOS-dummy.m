604e4ee7f40cf95030e219a88275e6aa6e77c4ff
Renan Machado <renan.mav@hotmail.com>
2019-12-30 00:04:54 -0300

    fix(nohoist): delete Pods folder

--- packages/mobile/ios/Pods/Target Support Files/Pods-Animavita-tvOS/Pods-Animavita-tvOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Animavita_tvOS : NSObject
-@end
-@implementation PodsDummy_Pods_Animavita_tvOS
-@end
--- packages/mobile/ios/Pods/Target Support Files/Pods-Animavita-tvOSTests/Pods-Animavita-tvOSTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Animavita_tvOSTests : NSObject
-@end
-@implementation PodsDummy_Pods_Animavita_tvOSTests
-@end
