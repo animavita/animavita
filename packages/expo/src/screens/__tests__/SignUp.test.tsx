d3e8eff5dfdb42f66769f1c8114405adac134ace
Renan Machado <renan.mav@hotmail.com>
2020-01-25 21:04:26 -0300

    feat(facebook-btn): add tests to fb btn

--- packages/expo/src/screens/__tests__/SignUp.test.tsx
+++ packages/expo/src/screens/__tests__/SignUp.test.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import {fireEvent} from '@testing-library/react-native';
 
 import SignUp from '../SignUp';
 
 import {Mount} from '../../../tests/helpers';
+import {FacebookButton} from '@animavita/ui/social';
 
 test('all elements are rendered', () => {
   const {queryByTestId} = Mount(<SignUp />);
@@ -21,4 +23,18 @@ test('all elements are rendered', () => {
   expect(wrapper).toContainElement(fbBtn);
   expect(wrapper).toContainElement(googleBtn);
   expect(wrapper).toContainElement(appleBtn);
+});
+
+test('if facebook button works', () => {
+  const loginWithFbMutation = jest.fn();
+
+  const {queryByTestId} = Mount(<FacebookButton testID="fb-btn" onPress={loginWithFbMutation} />);
+
+  const fbBtn = queryByTestId('fb-btn');
+
+  fireEvent.press(fbBtn!);
+
+  expect(loginWithFbMutation).toBeCalledTimes(1);
+  expect(loginWithFbMutation).not.toBeCalledTimes(0);
+  expect(loginWithFbMutation).not.toBeCalledTimes(2);
 });
