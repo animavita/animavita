09ef58fa9f29df176bfa86e2f0f71589c5f04ca6
Renan Machado <renan.mav@hotmail.com>
2020-01-27 00:01:45 -0300

    feat(graphql-tests): setup jest on graphql package

--- packages/expo/jest.config.js
+++ packages/expo/jest.config.js
@@ -1,4 +1,9 @@
+const pkg = require('./package');
+
 module.exports = {
+  rootDir: './',
+  name: pkg.name,
+  displayName: pkg.name.toUpperCase(),
   preset: 'jest-expo',
   browser: false,
   setupFilesAfterEnv: ['./tests/setup.ts'],
---
+++ packages/graphql/jest.config.js
@@ -0,0 +1,16 @@
+const pkg = require('./package');
+
+module.exports = {
+  rootDir: './',
+  name: pkg.name,
+  displayName: pkg.name.toUpperCase(),
+  testPathIgnorePatterns: ['/node_modules/', './build'],
+  coverageReporters: ['lcov', 'html'],
+  resetModules: false,
+  reporters: ['default'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)?$': '<rootDir>/tests/babel.transformer',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(js|jsx|ts|tsx)?$',
+  moduleFileExtensions: ['ts', 'js', 'tsx'],
+};
