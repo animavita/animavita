a69c60acf6a044727892624b3c9219884c1f404e
Renan Machado <renan.mav@hotmail.com>
2020-02-09 23:37:12 -0300

    feat(aws): grant access to Lambda upload files on S3

--- packages/graphql/src/modules/user/mutations/SaveFacebookUserMutation.ts
+++ packages/graphql/src/modules/user/mutations/SaveFacebookUserMutation.ts
@@ -116,7 +116,13 @@ export default mutationWithClientMutationId({
 async function uploadProfileImage(url: string, user: UserIncomplete) {
   const base64Image = await imageToBase64(url);
 
-  AWS.config.update({accessKeyId: AWS_ACCESS_KEY_ID, secretAccessKey: AWS_SECRET_ACCESS_KEY, region: AWS_REGION});
+  AWS.config.update({
+    accessKeyId: AWS_ACCESS_KEY_ID,
+    secretAccessKey: AWS_SECRET_ACCESS_KEY,
+    // needed for Lambda to be authorized to upload files on S3 (also Lambda role should be listed on bucket policy, see https://aws.amazon.com/pt/premiumsupport/knowledge-center/access-denied-lambda-s3-bucket/)
+    ...(process.env.NODE_ENV === 'production' ? {sessionToken: process.env.AWS_SESSION_TOKEN} : {}),
+    region: AWS_REGION,
+  });
 
   const buffer = Buffer.from(base64Image, 'base64');
 
