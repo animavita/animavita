eef82b6c25067b7ae77fc666995ab1e996896277
Renan Machado <renan.mav@hotmail.com>
2020-02-16 23:47:00 -0300

    feat(fb-login): login with fb on web

--- packages/expo/src/modules/signUp/ContinueWithFacebook.tsx
+++ packages/expo/src/modules/signUp/ContinueWithFacebook.tsx
@@ -2,13 +2,35 @@ import React, {useEffect, useState} from 'react';
 import {Platform, AsyncStorage} from 'react-native';
 import * as Facebook from 'expo-facebook';
 import {NavigationScreenProp, withNavigation} from 'react-navigation';
+import FacebookProvider, {Login} from 'react-facebook-sdk';
 
 import {FacebookButton} from '@animavita/ui/social';
 import {graphql, useMutation} from '@animavita/relay';
 
 import {changeShowBottomBar} from '../../utils/bottomBar';
 
-import {ContinueWithFacebookMutation as ContinueWithFacebookMutationType} from './__generated__/ContinueWithFacebookMutation.graphql';
+import {
+  ContinueWithFacebookMutation as ContinueWithFacebookMutationType,
+  ContinueWithFacebookMutationResponse,
+} from './__generated__/ContinueWithFacebookMutation.graphql';
+
+interface FacebookWebSuccessfulResponse {
+  profile: {
+    id: string;
+    first_name: string;
+    last_name: string;
+    name: string;
+    email: string;
+  };
+  tokenDetail: {
+    accessToken: string;
+    data_access_expiration_time: number;
+    expiresIn: number;
+    graphDomain: string;
+    signedRequest: string;
+    userID: string;
+  };
+}
 
 const ContinueWithFacebookMutation = graphql`
   mutation ContinueWithFacebookMutation($input: SaveFacebookUserInput!) {
@@ -47,7 +69,23 @@ const ContinueWithFacebook: React.FC<{navigation: NavigationScreenProp<any>}> = 
     Platform.OS !== 'web' && initializeFacebookSDK();
   }, []);
 
+  const onCompleted = async (data: ContinueWithFacebookMutationResponse) => {
+    changeFbLoginLoadingTo(false);
+    if (data.SaveFacebookUser && data.SaveFacebookUser.token) {
+      await AsyncStorage.setItem('token', data.SaveFacebookUser.token);
+      navigation.navigate('Home');
+    }
+  };
+
+  // TODO: show feedback of error
+  const onError = error => {
+    changeFbLoginLoadingTo(false);
+  };
+
   const loginWithFacebookMobile = async () => {
+    // prevent the user from firing too much requests
+    if (fbLoginIsLoading) return;
+
     changeFbLoginLoadingTo(true);
 
     const response = await Facebook.logInWithReadPermissionsAsync({
@@ -65,43 +103,48 @@ const ContinueWithFacebook: React.FC<{navigation: NavigationScreenProp<any>}> = 
             permissions,
           },
         },
-        onCompleted: async data => {
-          if (data.SaveFacebookUser && data.SaveFacebookUser.token) {
-            await AsyncStorage.setItem('token', data.SaveFacebookUser.token);
-            navigation.navigate('Home');
-          }
-          changeFbLoginLoadingTo(false);
-        },
-        onError: () => {
-          changeFbLoginLoadingTo(false);
-        },
+        onCompleted,
+        onError,
       });
     } else {
       changeFbLoginLoadingTo(false);
     }
   };
 
-  // TODO: implement login with facebook on web
-  const loginWithFacebookWeb = async () => {
-    changeFbLoginLoadingTo(true);
-
-    setTimeout(() => {
-      navigation.navigate('Home');
-    }, 3000);
-  };
-
-  const handleFacebookLogin = async () => {
+  const loginWithFacebookWeb = async (data: FacebookWebSuccessfulResponse) => {
     // prevent the user from firing too much requests
     if (fbLoginIsLoading) return;
 
-    if (Platform.OS !== 'web') {
-      await loginWithFacebookMobile();
-    } else {
-      await loginWithFacebookWeb();
-    }
+    changeFbLoginLoadingTo(true);
+
+    saveFacebookUser({
+      variables: {
+        input: {
+          token: data.tokenDetail.accessToken,
+          expires: data.tokenDetail.expiresIn,
+          permissions: ['public_profile', 'email'],
+        },
+      },
+      onCompleted,
+      onError,
+    });
   };
 
-  return <FacebookButton testID="fb-btn" onPress={handleFacebookLogin} />;
+  const handleLoginWithFacebookWebError = error => {
+    changeFbLoginLoadingTo(false);
+  };
+
+  if (Platform.OS === 'web') {
+    return (
+      <FacebookProvider appId="877731272663210">
+        <Login scope="public_profile,email" onResponse={loginWithFacebookWeb} onError={handleLoginWithFacebookWebError}>
+          <FacebookButton testID="fb-btn" />
+        </Login>
+      </FacebookProvider>
+    );
+  } else {
+    return <FacebookButton testID="fb-btn" onPress={loginWithFacebookMobile} />;
+  }
 };
 
 export default withNavigation(ContinueWithFacebook);
