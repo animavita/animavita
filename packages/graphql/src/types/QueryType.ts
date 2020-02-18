3bba1cafc629c2a6499629ac1748dc30caa4b40d
Renan Machado <renan.mav@hotmail.com>
2020-02-17 23:42:33 -0300

    feat(user): add me query

--- packages/graphql/src/types/QueryType.ts
+++ packages/graphql/src/types/QueryType.ts
@@ -1,27 +1,15 @@
-import {GraphQLID, GraphQLObjectType, GraphQLNonNull} from 'graphql';
-import {fromGlobalId} from 'graphql-relay';
+import {GraphQLObjectType} from 'graphql';
 
 import {GraphQLContext} from '../types';
 import {nodeField} from '../interfaces/NodeInterface';
-import {UserLoader} from '../loaders';
-import UserType from '../modules/user/UserType';
+
+import {userQuerys} from '../modules/user/UserQuery';
 
 export default new GraphQLObjectType<any, GraphQLContext, any>({
   name: 'Query',
   description: 'The root of all queries',
   fields: () => ({
     node: nodeField,
-    user: {
-      type: UserType,
-      args: {
-        id: {
-          type: new GraphQLNonNull(GraphQLID),
-        },
-      },
-      resolve: (obj, args, context) => {
-        const {id} = fromGlobalId(args.id);
-        return UserLoader.load(context, id);
-      },
-    },
+    ...userQuerys,
   }),
 });
