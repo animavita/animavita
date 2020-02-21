65db18baf7c2df3c29fbbb135483ae6b543f5c5a
Renan Machado <renan.mav@hotmail.com>
2020-02-21 11:54:58 -0300

    fix(actions): add skiping workflows and add logout button

--- packages/expo/src/modules/home/Home.tsx
+++ packages/expo/src/modules/home/Home.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import styled from 'styled-components/native';
+import {Button, AsyncStorage} from 'react-native';
 
 const Wrapper = styled.SafeAreaView``;
 
 const Home: React.FC = () => {
-  return <Wrapper />;
+  return (
+    <Wrapper>
+      <Button title="Logout" onPress={() => AsyncStorage.clear()} />
+    </Wrapper>
+  );
 };
 
 export default Home;
