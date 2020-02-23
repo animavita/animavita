2a52ffa3fd34761e663b3efeab2eb42dee63a3fe
Renan Machado <renan.mav@hotmail.com>
2020-02-23 14:02:59 -0300

    feat(web): move to one script

--- scripts/DeployStaging.sh
+++ scripts/DeployStaging.sh
@@ -1,6 +1,6 @@
-yarn workspace @animavita/graphql prepare:build
 yarn workspace @animavita/relay prepare:build
 yarn workspace @animavita/expo prepare:build
+yarn workspace @animavita/graphql prepare:build
 
 yarn workspace @animavita/aws deploy AnimavitaStagingGraphQLStack \
   -c mode=staging \
@@ -8,4 +8,10 @@ yarn workspace @animavita/aws deploy AnimavitaStagingGraphQLStack \
   -c certificateArn=$CERTIFICATE_ARN \
   -c JWT_KEY=$JWT_KEY \
   -c MONGO_URI=$MONGO_URI \
+  --require-approval=never
+
+yarn workspace @animavita/aws deploy AnimavitaStagingWebStack \
+  -c mode=staging \
+  -c hostedZoneId=$HOSTED_ZONE_ID \
+  -c certificateArn=$CERTIFICATE_ARN \
   --require-approval=never
--- scripts/DeployWebStaging.sh
+++
@@ -1,8 +0,0 @@
-yarn workspace @animavita/relay prepare:build
-yarn workspace @animavita/expo prepare:build
-
-yarn workspace @animavita/aws deploy AnimavitaStagingWebStack \
-  -c mode=staging \
-  -c hostedZoneId=$HOSTED_ZONE_ID \
-  -c certificateArn=$CERTIFICATE_ARN \
-  --require-approval=never
