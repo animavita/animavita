b6fc972d303d400a99737125b090b1b9dc2f10ed
Renan Machado <renan.mav@hotmail.com>
2020-02-23 13:21:16 -0300

    feat(web): create workflow for deploy of frontend

--- packages/aws/stacks/WebStack.ts
+++ packages/aws/stacks/WebStack.ts
@@ -1,13 +1,15 @@
 import * as CDK from '@aws-cdk/core';
 import * as S3 from '@aws-cdk/aws-s3';
 import * as S3Deployment from '@aws-cdk/aws-s3-deployment';
+import * as Cloudfront from '@aws-cdk/aws-cloudfront';
+import * as Route53 from '@aws-cdk/aws-route53';
+import * as Route53Targets from '@aws-cdk/aws-route53-targets';
 
 import {ModeStack} from '../helpers';
 
 export class WebStack extends ModeStack {
   public readonly domainName = 'animavita.site';
   public readonly hostedZoneId: string = this.node.tryGetContext('hostedZoneId');
-  public readonly graphqlWhitecard = this.mode === 'production' ? 'graphql' : `graphql-${this.mode}`;
   public readonly certificateArn: string = this.node.tryGetContext('certificateArn');
 
   constructor(app: CDK.App, id: string) {
@@ -22,6 +24,68 @@ export class WebStack extends ModeStack {
     new S3Deployment.BucketDeployment(this, 'AnimavitaWebDeploy', {
       sources: [S3Deployment.Source.asset('../expo/web-build')],
       destinationBucket: bucket,
+    });
+
+    const viewerCertificate = Cloudfront.ViewerCertificate.fromAcmCertificate(
+      {
+        stack: this,
+        node: this.node,
+        certificateArn: this.certificateArn,
+      },
+      {
+        aliases: [this.domainName, `www.${this.domainName}`],
+        sslMethod: Cloudfront.SSLMethod.SNI,
+      },
+    );
+
+    const cdn = new Cloudfront.CloudFrontWebDistribution(this, 'AnimavitaWebCDN', {
+      httpVersion: Cloudfront.HttpVersion.HTTP1_1,
+      priceClass: Cloudfront.PriceClass.PRICE_CLASS_100,
+      viewerProtocolPolicy: Cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      defaultRootObject: 'index.html',
+      viewerCertificate,
+      originConfigs: [
+        {
+          customOriginSource: {
+            domainName: bucket.bucketWebsiteDomainName,
+          },
+          behaviors: [
+            {
+              allowedMethods: Cloudfront.CloudFrontAllowedMethods.GET_HEAD_OPTIONS,
+              compress: true,
+              isDefaultBehavior: true,
+              forwardedValues: {
+                queryString: true,
+                cookies: {forward: 'all'},
+              },
+            },
+            {
+              allowedMethods: Cloudfront.CloudFrontAllowedMethods.GET_HEAD_OPTIONS,
+              compress: true,
+              pathPattern: '/*',
+              forwardedValues: {
+                queryString: true,
+                cookies: {forward: 'all'},
+              },
+            },
+          ],
+        },
+      ],
+    });
+
+    const hostedZone = Route53.HostedZone.fromHostedZoneAttributes(this, 'AnimavitaHostedZone', {
+      zoneName: this.domainName,
+      hostedZoneId: this.hostedZoneId,
+    });
+
+    new Route53.ARecord(this, 'AnimavitaWebAlias', {
+      zone: hostedZone,
+      target: Route53.RecordTarget.fromAlias(new Route53Targets.CloudFrontTarget(cdn)),
+    });
+    new Route53.ARecord(this, 'AnimavitaWebWWWAlias', {
+      recordName: 'www',
+      zone: hostedZone,
+      target: Route53.RecordTarget.fromAlias(new Route53Targets.CloudFrontTarget(cdn)),
     });
   }
 }
---
+++ packages/expo/scripts/prepareBuild.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# This script must be executed from the repo root folder
+
+if ! command -v aws >/dev/null; then
+  echo "ERROR: You need to install aws cli. Try \"pip install awscli\" if you have python on your env"
+  exit
+fi
+
+# Create environment.ts
+cat <<EOF >./environment.ts
+import Constants from 'expo-constants';
+
+const ENV = {
+  dev: {
+    fbAppID: $FB_APP_ID,
+    fbAppName: $FB_APP_NAME,
+  },
+  staging: {
+    fbAppID: $FB_APP_ID,
+    fbAppName: $FB_APP_NAME,
+  },
+  prod: {
+    fbAppID: $FB_APP_ID,
+    fbAppName: $FB_APP_NAME,
+  },
+};
+
+const getEnvVars = (env = Constants.manifest.releaseChannel) => {
+  // What is __DEV__ ?
+  // This variable is set to true when react-native is running in Dev mode.
+  // __DEV__ is true when run locally, but false when published.
+  if (__DEV__) {
+    return ENV.dev;
+  } else if (env === 'staging') {
+    return ENV.staging;
+  } else {
+    return ENV.prod;
+  }
+};
+
+export default getEnvVars;
+EOF
+
+# Build
+rm -rf web-build
+yarn build:web
