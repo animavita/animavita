9cc660720ad573a38ec3d470900c2cc77ee72853
Petrus Pierre <petrus.pierre@codeminer42.com>
2020-12-29 18:26:52 -0300

    chore: change in jest config

--- packages/expo/jest.config.js
+++ packages/expo/jest.config.js
@@ -6,4 +6,7 @@ module.exports = {
   displayName: pkg.name.toUpperCase(),
   preset: 'jest-expo',
   setupFilesAfterEnv: ['./tests/setup.ts'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@sentry/.*)',
+  ],
 };
