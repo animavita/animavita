80d7d33e668513f00c856068a6dac2837c073624
Gabriel Belgamo <ggbelgamo@outlook.com>
2021-01-08 01:32:33 -0300

    fix: fix images doesn't appear on android

--- packages/expo/metro.config.js
+++ packages/expo/metro.config.js
@@ -1,3 +1,28 @@
 const {createMetroConfiguration} = require('expo-yarn-workspaces');
 
-module.exports = createMetroConfiguration(__dirname);
+const config = createMetroConfiguration(__dirname);
+
+config.server = {
+  enhanceMiddleware: middleware => {
+    return (req, res, next) => {
+      const assets = '/ui/assets/';
+      if (req.url.startsWith(assets)) {
+        // At the beginning of the path to your assets should always be "/assets/"
+        // Next exit from the current directory
+        req.url = req.url.replace(assets, '/assets/../ui/assets/');
+      }
+
+      // Fix showing back icon on android
+      // This is necessary if you are working with react-navigation and
+      // it is installed globally for monorepo.
+      const otherAssets = '/node_modules/';
+      if (req.url.startsWith(otherAssets)) {
+        req.url = req.url.replace(otherAssets, '/assets/../../node_modules/');
+      }
+
+      return middleware(req, res, next);
+    };
+  },
+};
+
+module.exports = config;
