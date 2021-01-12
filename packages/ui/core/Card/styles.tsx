2790d91fda3d2c4f71ee26ebcd57300c693a9890
BeAmarawn <bernardoamaralmarinho@gmail.com>
2021-01-12 16:18:43 -0300

    feat: add structure and styles of card components

---
+++ packages/ui/core/Card/styles.tsx
@@ -0,0 +1,35 @@
+import styled from 'styled-components/native';
+
+export const Image = styled.Image`
+  width: null;
+  height: null;
+  border-radius: 8;
+`;
+export const Overlay = styled.View`
+  flex: 1;
+  justify-content: space-between;
+  padding: 16;
+`;
+export const Headers = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+`;
+export const Footer = styled.View`
+  flex-direction: row;
+`;
+export const Name = styled.Text`
+  color: white;
+  font-size: 32;
+`;
+
+export const LikeLabel = styled.Text`
+  font-size: 32;
+  color: #6ee3b4;
+  font-weight: bold;
+`;
+
+export const NopeLabel = styled.Text`
+  font-size: 32;
+  color: #ec5288;
+  font-weight: bold;
+`;
