d5057eb32d325056297ffe93e138615af49b0a72
BeAmarawn <bernardoamaralmarinho@gmail.com>
2021-01-12 16:18:43 -0300

    feat: add structure and styles of card components

--- packages/expo/src/modules/home/Home.tsx
+++ packages/expo/src/modules/home/Home.tsx
@@ -1,23 +1,45 @@
 import React from 'react';
-import {Button, AsyncStorage} from 'react-native';
+import {AsyncStorage} from 'react-native';
 import {px2ddp, useTheme} from '@animavita/theme';
 import {Background} from '@animavita/ui/layout';
 import {TabBar} from '@animavita/ui/core';
 import styled from 'styled-components/native';
 
+import Button from '../../../../ui/core/Button/Button';
+
 const Wrapper = styled.View`
   margin: 0 ${px2ddp(10)}px;
 `;
 const ButtonsWrapper = styled.View`
-  height: 100%;
-  justify-content: center;
+  flex-direction: row;
+  justify-content: space-between;
+  margin-top: 20px;
 `;
 
 const Home: React.FC = () => {
   const theme = useTheme();
+
+  const Pets = [
+    {
+      name: 'Thor',
+      age: 2,
+    },
+    {
+      name: 'Bob',
+      age: 2,
+    },
+    {
+      name: 'Tim',
+      age: 2,
+    },
+  ];
   return (
     <Background>
       <Wrapper>
+        {/*  <ButtonsWrapper>
+          <Button title="Logout" onPress={() => AsyncStorage.clear()} />
+          <Button title="Change theme" onPress={() => theme.changeTheme()} />
+        </ButtonsWrapper> */}
         <TabBar
           items={[
             {displayName: 'adoções', key: 'adocoes'},
@@ -27,8 +49,8 @@ const Home: React.FC = () => {
           onPress={() => null}
         />
         <ButtonsWrapper>
-          <Button title="Logout" onPress={() => AsyncStorage.clear()} />
-          <Button title="Change theme" onPress={() => theme.changeTheme()} />
+          <Button size="small" rounded gradient text="Filtrar" onPress={() => AsyncStorage.clear()} />
+          <Button size="small" rounded gradient text="Cadastrar para adoção" onPress={() => theme.changeTheme()} />
         </ButtonsWrapper>
       </Wrapper>
     </Background>
