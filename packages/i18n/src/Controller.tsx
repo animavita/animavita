a16b625d40eae859b0b2eeeb7ccce6adeb08926e
Petrus Pierre <petrus.pierre@codeminer42.com>
2021-04-29 10:40:28 -0300

    Create i18n controller

---
+++ packages/i18n/src/Controller.tsx
@@ -0,0 +1,41 @@
+import React, {useState, useEffect, useContext, createContext} from 'react';
+import i18n from 'i18n-js';
+
+import locales from '../locales';
+
+interface I18nContextProps {
+  t: (key: string, options: object) => string;
+  setLocale: (locale: string) => void;
+  locale: string;
+}
+
+const I18nContext = createContext<I18nContextProps | undefined>(undefined);
+
+const I18nProvider: React.FC = ({children}) => {
+  const [locale, setLocale] = useState<string>('en');
+
+  i18n.fallbacks = true;
+  i18n.defaultLocale = 'pt-BR';
+
+  useEffect(() => {
+    i18n.locale = locale;
+  }, [locale]);
+
+  useEffect(() => {
+    i18n.translations = locales;
+  }, []);
+
+  return <I18nContext.Provider value={{t: i18n.t, setLocale, locale}}>{children}</I18nContext.Provider>;
+};
+
+const useI18nController = () => {
+  const context = useContext(I18nContext);
+
+  if (!context) {
+    throw new Error('useI18nController must be used within an I18nProvider.');
+  }
+
+  return context;
+};
+
+export {I18nProvider, useI18nController};
