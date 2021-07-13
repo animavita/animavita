22ad9bc0f4cd693e4ce84b352b9c34a6d8a8134d
Beigelman <daniel.beigelman@codeminer42.com>
2021-07-13 10:44:42 -0300

    fix(): user tests organization

--- packages/graphql/src/modules/user/app/authenticateFacebookUser.spec.ts
+++ packages/graphql/src/modules/user/app/authenticateFacebookUser.spec.ts
@@ -57,141 +57,147 @@ const setUp = () => {
 };
 
 describe('AuthenticateFacebookUser', () => {
-  it('should be able to authenticate', async () => {
-    const {authenticateUser} = setUp();
+  describe('When logging in for the first time', () => {
+    it('authenticates', async () => {
+      const {authenticateUser} = setUp();
 
-    const {user, token} = await authenticateUser(authenticateUserParams);
+      const {user, token} = await authenticateUser(authenticateUserParams);
 
-    expect(user).toBeInstanceOf(User);
-    expect(user).toEqual({
-      id: 'fake-uuid',
-      name: 'fake-user',
-      emails: [
-        {
-          email: 'mysocialemail@fake.com',
-          providedBy: 'facebook',
-        },
-      ],
-      profileImages: [
-        {
-          providedBy: 'facebook',
-          url: 'fake-token/profilePicture.jpeg',
-        },
-      ],
-      providersIds: [
-        {
-          id: 'socialId-fake-token',
-          providedBy: 'facebook',
-        },
-      ],
+      expect(user).toBeInstanceOf(User);
+      expect(user).toEqual({
+        id: 'fake-uuid',
+        name: 'fake-user',
+        emails: [
+          {
+            email: 'mysocialemail@fake.com',
+            providedBy: 'facebook',
+          },
+        ],
+        profileImages: [
+          {
+            providedBy: 'facebook',
+            url: 'fake-token/profilePicture.jpeg',
+          },
+        ],
+        providersIds: [
+          {
+            id: 'socialId-fake-token',
+            providedBy: 'facebook',
+          },
+        ],
+      });
+      expect(token).toMatch(/(JWT )\w+\.\w+\.\w+/g);
     });
-    expect(token).toMatch(/(JWT )\w+\.\w+\.\w+/g);
+
+    it('authenticates even with no user photo is provided', async () => {
+      const {facebookRepository, authenticateUser} = setUp();
+
+      jest.spyOn(facebookRepository, 'getUserProfileImage').mockImplementationOnce(async () => {
+        return null;
+      });
+
+      const {user, token} = await authenticateUser(authenticateUserParams);
+
+      expect(user).toBeInstanceOf(User);
+      expect(user).toEqual({
+        id: 'fake-uuid',
+        name: 'fake-user',
+        emails: [
+          {
+            email: 'mysocialemail@fake.com',
+            providedBy: 'facebook',
+          },
+        ],
+        profileImages: [],
+        providersIds: [
+          {
+            id: 'socialId-fake-token',
+            providedBy: 'facebook',
+          },
+        ],
+      });
+      expect(token).toMatch(/(JWT )\w+\.\w+\.\w+/g);
+    });
   });
 
-  it('should be able to authenticate with no user photo', async () => {
-    const {facebookRepository, authenticateUser} = setUp();
+  describe('When logging in again with an already existing user', () => {
+    it('authenticates', async () => {
+      const {authenticateUser, userRepository} = setUp();
 
-    jest.spyOn(facebookRepository, 'getUserProfileImage').mockImplementationOnce(async () => {
-      return null;
+      await userRepository.createUser(fakeUser);
+
+      const {user} = await authenticateUser(authenticateUserParams);
+
+      expect(user.id).toBe('fake-uuid');
+      expect(user.emails[0].email).toBe('mysocialemail@fake.com');
     });
 
-    const {user, token} = await authenticateUser(authenticateUserParams);
+    it('updates name if the name has changed', async () => {
+      const {authenticateUser, userRepository} = setUp();
 
-    expect(user).toBeInstanceOf(User);
-    expect(user).toEqual({
-      id: 'fake-uuid',
-      name: 'fake-user',
-      emails: [
-        {
-          email: 'mysocialemail@fake.com',
-          providedBy: 'facebook',
-        },
-      ],
-      profileImages: [],
-      providersIds: [
-        {
-          id: 'socialId-fake-token',
-          providedBy: 'facebook',
-        },
-      ],
+      await userRepository.createUser({
+        ...fakeUser,
+        name: 'old-fake-user',
+      });
+
+      const {user} = await authenticateUser(authenticateUserParams);
+
+      expect(user.name).toBe('fake-user');
     });
-    expect(token).toMatch(/(JWT )\w+\.\w+\.\w+/g);
+
+    it('updates provider id if the id has changed', async () => {
+      const {authenticateUser, userRepository} = setUp();
+
+      await userRepository.createUser({
+        ...fakeUser,
+        providersIds: [
+          {
+            id: 'old-socialId-fake-token',
+            providedBy: 'facebook',
+          },
+        ],
+      });
+
+      const {user} = await authenticateUser(authenticateUserParams);
+
+      expect(user.providersIds[0].id).toBe('socialId-fake-token');
+    });
+
+    it('updates profilePhoto if none was provided before', async () => {
+      const {authenticateUser, userRepository} = setUp();
+
+      await userRepository.createUser({
+        ...fakeUser,
+        profileImages: [],
+      });
+
+      const {user} = await authenticateUser(authenticateUserParams);
+
+      expect(user.providersIds[0].id).toBe('socialId-fake-token');
+    });
+
+    it('throws an error if it is not able to find the user when updated', async () => {
+      const {authenticateUser, userRepository} = setUp();
+
+      jest.spyOn(userRepository, 'findById').mockImplementationOnce(async () => {
+        return null;
+      });
+
+      await userRepository.createUser(fakeUser);
+
+      await expect(authenticateUser(authenticateUserParams)).rejects.toBeInstanceOf(Error);
+    });
   });
 
-  it('should be able to login again if a user already exists', async () => {
-    const {authenticateUser, userRepository} = setUp();
+  describe('When the social provider does not return any data', () => {
+    it('throws an error', async () => {
+      const {authenticateUser, facebookRepository} = setUp();
 
-    await userRepository.createUser(fakeUser);
+      jest.spyOn(facebookRepository, 'getUser').mockImplementationOnce(async () => {
+        return null;
+      });
 
-    const {user} = await authenticateUser(authenticateUserParams);
-
-    expect(user.id).toBe('fake-uuid');
-    expect(user.emails[0].email).toBe('mysocialemail@fake.com');
-  });
-
-  it('should update name if the name has changed', async () => {
-    const {authenticateUser, userRepository} = setUp();
-
-    await userRepository.createUser({
-      ...fakeUser,
-      name: 'old-fake-user',
+      await expect(authenticateUser(authenticateUserParams)).rejects.toBeInstanceOf(Error);
     });
-
-    const {user} = await authenticateUser(authenticateUserParams);
-
-    expect(user.name).toBe('fake-user');
-  });
-
-  it('should update provider id if the id has changed', async () => {
-    const {authenticateUser, userRepository} = setUp();
-
-    await userRepository.createUser({
-      ...fakeUser,
-      providersIds: [
-        {
-          id: 'old-socialId-fake-token',
-          providedBy: 'facebook',
-        },
-      ],
-    });
-
-    const {user} = await authenticateUser(authenticateUserParams);
-
-    expect(user.providersIds[0].id).toBe('socialId-fake-token');
-  });
-
-  it('should update profilePhoto if none was provided before', async () => {
-    const {authenticateUser, userRepository} = setUp();
-
-    await userRepository.createUser({
-      ...fakeUser,
-      profileImages: [],
-    });
-
-    const {user} = await authenticateUser(authenticateUserParams);
-
-    expect(user.providersIds[0].id).toBe('socialId-fake-token');
-  });
-
-  it('should throw an error if it is not able to find the user when updated', async () => {
-    const {authenticateUser, userRepository} = setUp();
-
-    jest.spyOn(userRepository, 'findById').mockImplementationOnce(async () => {
-      return null;
-    });
-
-    await userRepository.createUser(fakeUser);
-
-    await expect(authenticateUser(authenticateUserParams)).rejects.toBeInstanceOf(Error);
-  });
-
-  it('should throw an error if no data if return from social provider', async () => {
-    const {authenticateUser, facebookRepository} = setUp();
-
-    jest.spyOn(facebookRepository, 'getUser').mockImplementationOnce(async () => {
-      return null;
-    });
-
-    await expect(authenticateUser(authenticateUserParams)).rejects.toBeInstanceOf(Error);
   });
 });
