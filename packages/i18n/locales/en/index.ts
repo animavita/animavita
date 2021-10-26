07044836dd64fe57b263af98ae7304f71fa64b80
Petrus Bento <petrus@intellimize.com>
2021-10-26 19:43:06 -0300

    i18n: update adoption copy

--- packages/i18n/locales/en/index.ts
+++ packages/i18n/locales/en/index.ts
@@ -1,11 +1,13 @@
 import home from './home.json';
 import tab_bar from './tab_bar.json';
 import sign_up from './sign_up.json';
+import register_adoption from './register_adoption.json';
 
 const en = {
   home,
   tab_bar,
   sign_up,
+  register_adoption,
 };
 
 export default en;
