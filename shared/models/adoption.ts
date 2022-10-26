3de7bb1c5402f4041cea81da98cab801c599a500
Gabriel Belgamo <ggbelgamo@outlook.com>
2022-10-26 11:22:30 -0300

    feat: set up shared/models

---
+++ shared/models/adoption.ts
@@ -0,0 +1,16 @@
+export type AnimalGendersType = "male" | "female";
+export type AnimalType = "dog" | "cat" | "other";
+export type AnimalSizesType = "small" | "medium" | "big";
+
+export type AdoptionType = {
+  name: string;
+  gender: AnimalGendersType;
+  breed: string;
+  type: AnimalType;
+  age: number;
+  size: AnimalSizesType;
+  observations: string;
+  photos: string[];
+};
+
+export default AdoptionType;
