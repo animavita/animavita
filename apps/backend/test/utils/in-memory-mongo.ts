38ec942b378da5d4e58a9201f58a33e9a01d93f7
Gabriel Belgamo <ggbelgamo@outlook.com>
2022-11-02 19:25:54 -0300

    chore: setup mongodb-memory-server

---
+++ apps/backend/test/utils/in-memory-mongo.ts
@@ -0,0 +1,20 @@
+import { MongooseModule, MongooseModuleOptions } from '@nestjs/mongoose';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+
+let mongod: MongoMemoryServer;
+
+export const rootMongooseTestModule = (options: MongooseModuleOptions = {}) =>
+  MongooseModule.forRootAsync({
+    useFactory: async () => {
+      mongod = await MongoMemoryServer.create();
+      const mongoUri = await mongod.getUri();
+      return {
+        uri: mongoUri,
+        ...options,
+      };
+    },
+  });
+
+export const closeInMongodConnection = async () => {
+  if (mongod) await mongod.stop();
+};
