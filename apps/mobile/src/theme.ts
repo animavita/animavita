cc20afccf0134cd08cf7442afb543b4b7082c93b
joaoGabriel55 <j.quaresmasantos_98@hotmail.com>
2022-11-07 09:05:49 -0300

    chore: fix theme

--- apps/mobile/src/theme.ts
+++ apps/mobile/src/theme.ts
@@ -3,9 +3,14 @@ import { MD3LightTheme as DefaultTheme } from "react-native-paper";
 const theme = {
   ...DefaultTheme,
   colors: {
-    ...DefaultTheme.colors,
-    primary: "#6bc9b3",
-    secondary: "##7cdb90",
+    primary: "#51C6B5",
+    onPrimary: "rgb(255, 255, 255)",
+    primaryContainer: "rgb(121, 248, 219)",
+    onPrimaryContainer: "rgb(0, 32, 26)",
+    secondary: "#7cdb90",
+    onSecondary: "rgb(255, 255, 255)",
+    secondaryContainer: "rgb(151, 247, 170)",
+    onSecondaryContainer: "rgb(0, 33, 10)",
   },
 };
 
