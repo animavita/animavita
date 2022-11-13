1af2948cf9356f65429d6bbc72dfc2f39da413ca
joaoGabriel55 <j.quaresmasantos_98@hotmail.com>
2022-11-13 10:38:34 -0300

    feat: add i18n locale

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -1,5 +1,7 @@
 import { QueryClientProvider } from "@tanstack/react-query";
+import { I18nextProvider } from "react-i18next";
 import { Provider as PaperProvider } from "react-native-paper";
+import { initI18n } from "./src/i18n/i18n.config";
 import MainNavigator from "./src/navigation/main-navigator";
 import queryClient from "./src/services/query-client-instance";
 import theme from "./src/theme";
@@ -8,7 +10,9 @@ export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <PaperProvider theme={theme}>
-        <MainNavigator />
+        <I18nextProvider i18n={initI18n("pt-BR")}>
+          <MainNavigator />
+        </I18nextProvider>
       </PaperProvider>
     </QueryClientProvider>
   );
