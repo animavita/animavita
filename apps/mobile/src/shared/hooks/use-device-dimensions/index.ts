147ff80ef173c8444bbb0b42d31ffd2275c2a156
joaoGabriel55 <j.quaresmasantos_98@hotmail.com>
2022-12-26 21:16:39 -0300

    feat: add useDeviceDimensions hook

---
+++ apps/mobile/src/shared/hooks/use-device-dimensions/index.ts
@@ -0,0 +1,7 @@
+import { Dimensions } from "react-native";
+
+export default function useDeviceDimensions() {
+  const { width, height } = Dimensions.get("window");
+
+  return { deviceWidth: width, deviceHeight: height };
+}
