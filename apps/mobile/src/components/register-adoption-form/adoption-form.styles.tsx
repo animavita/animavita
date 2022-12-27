a624eae1f1b8c5b9ab0e6e2c8c67bd66bff4df15
joaoGabriel55 <j.quaresmasantos_98@hotmail.com>
2022-12-26 21:33:32 -0300

    chore: remove register-adoption-form/adoption-form.styles.tsx

--- apps/mobile/src/components/register-adoption-form/adoption-form.styles.tsx
+++
@@ -1,7 +0,0 @@
-import { moderateScale } from 'react-native-size-matters';
-import styled from 'styled-components/native';
-
-export const Icon = styled.Image`
-  width: ${moderateScale(36)}px;
-  height: ${moderateScale(36)}px;
-`;
