70be50a6fc2d80d8e24024d808c5f6d5defc3db2
JVBorges <joao-salves@hotmail.com>
2023-02-02 22:43:36 -0300

    refactor(auth): add setup function to e2e auth test

--- apps/backend/test/e2e/auth.e2e-spec.ts
+++ apps/backend/test/e2e/auth.e2e-spec.ts
@@ -1,4 +1,3 @@
-import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { ConfigModule } from '@nestjs/config';
 
@@ -13,27 +12,36 @@ import { AuthModule } from '../../src/auth/auth.module';
 import { AuthService } from '../../src/auth/auth.service';
 import { UserModule } from '../../src/user/user.module';
 
+const setup = async () => {
+  const moduleFixture: TestingModule = await Test.createTestingModule({
+    imports: [
+      rootMongooseTestModule(),
+      UserModule,
+      AuthModule,
+      ConfigModule.forRoot({ isGlobal: true }),
+    ],
+  }).compile();
+
+  const app = moduleFixture.createNestApplication();
+  const service = moduleFixture.get<AuthService>(AuthService);
+
+  await app.init();
+
+  return {
+    app,
+    service,
+  };
+};
+
+const tomorrow = () => {
+  const date = new Date();
+  return date.setDate(date.getDate() + 1);
+};
+
 describe('Authentication (e2e)', () => {
-  let app: INestApplication;
-  let service: AuthService;
-
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [
-        rootMongooseTestModule(),
-        UserModule,
-        AuthModule,
-        ConfigModule.forRoot({ isGlobal: true }),
-      ],
-    }).compile();
-
-    app = moduleFixture.createNestApplication();
-    service = moduleFixture.get<AuthService>(AuthService);
-
-    await app.init();
-  });
-
   it('/POST auth/signUp', async () => {
+    const { app } = await setup();
+
     const { body } = await request(app.getHttpServer())
       .post('/api/v1/auth/signUp')
       .send(userMock)
@@ -42,9 +50,13 @@ describe('Authentication (e2e)', () => {
     expect(body).toStrictEqual({
       message: 'User created successfully',
     });
+
+    await app.close();
   });
 
   it('/POST auth/signIn', async () => {
+    const { app, service } = await setup();
+
     await service.signUp(userMock);
 
     const { body } = await request(app.getHttpServer())
@@ -61,9 +73,13 @@ describe('Authentication (e2e)', () => {
         refreshToken: expect.any(String),
       }),
     );
+
+    await app.close();
   });
 
   it('/GET auth/logout', async () => {
+    const { app, service } = await setup();
+
     const { accessToken } = await service.signUp(userMock).then(() =>
       service.signIn({
         email: userMock.email,
@@ -75,9 +91,13 @@ describe('Authentication (e2e)', () => {
       .get('/api/v1/auth/logout')
       .auth(accessToken, { type: 'bearer' })
       .expect(200);
+
+    await app.close();
   });
 
   it('/GET auth/refresh', async () => {
+    const { app, service } = await setup();
+
     const { accessToken, refreshToken } = await service
       .signUp(userMock)
       .then(() =>
@@ -87,8 +107,10 @@ describe('Authentication (e2e)', () => {
         }),
       );
 
-    // I need to wait a second because if I don't the tokens will be the same
-    await new Promise((r) => setTimeout(r, 1000));
+    jest.useFakeTimers({
+      doNotFake: ['nextTick'],
+      now: tomorrow(),
+    });
 
     const { body } = await request(app.getHttpServer())
       .get('/api/v1/auth/refresh')
@@ -99,10 +121,11 @@ describe('Authentication (e2e)', () => {
       accessToken,
       refreshToken,
     });
+
+    await app.close();
   });
 
   afterEach(async () => {
-    await app.close();
     await closeInMongodConnection();
   });
 });
