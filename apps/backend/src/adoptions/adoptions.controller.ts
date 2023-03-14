2e4e3e0ef73f5814200a865c2f6515ec89213bf4
Bhaskar Sharma <imbhaskaran@gmail.com>
2023-03-14 08:01:58 +0530

    protected adoptions api

--- apps/backend/src/adoptions/adoptions.controller.ts
+++ apps/backend/src/adoptions/adoptions.controller.ts
@@ -3,6 +3,7 @@ import {
   adoptionValidationSchema,
   createValidationSchema,
 } from '@animavita/validation-schemas';
+import { UseGuards } from '@nestjs/common';
 import {
   Body,
   Controller,
@@ -15,29 +16,34 @@ import {
 } from '@nestjs/common';
 import { JoiValidationPipe } from '../pipes/joi-validation-pipe';
 import { AdoptionsService } from './adoptions.service';
+import { AccessTokenGuard } from 'src/guards/accessToken.guard';
 
 @Controller('api/v1/adoptions')
 export class AdoptionsController {
   constructor(private readonly adoptionsService: AdoptionsService) {}
-
+  
   @Post()
+  @UseGuards(AccessTokenGuard)
   @UsePipes(new JoiValidationPipe(createValidationSchema))
   async createAdoption(@Body() adoption: AdoptionType) {
     return this.adoptionsService.createAdoption(adoption);
   }
 
   @Patch()
+  @UseGuards(AccessTokenGuard)
   @UsePipes(new JoiValidationPipe(adoptionValidationSchema))
   async updateAdoption(@Body() adoption: AdoptionType) {
     return this.adoptionsService.updateAdoption(adoption);
   }
 
   @Get()
-  async findAll() {
+  @UseGuards(AccessTokenGuard)
+  async findAll(): Promise<AdoptionType[]> {
     return this.adoptionsService.findAll();
   }
 
   @Delete(':id')
+  @UseGuards(AccessTokenGuard)
   async deleteAdoption(@Param('id') adoptionId: string) {
     return this.adoptionsService.deleteAdoption(adoptionId);
   }
