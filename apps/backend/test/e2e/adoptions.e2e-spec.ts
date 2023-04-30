5a4fc63a9d825c047261a52be90370046c6c9335
Gabriel Belgamo <ggbelgamo@outlook.com>
2023-04-30 20:48:23 -0300

    test: update e2e tests

--- apps/backend/test/e2e/adoptions.e2e-spec.ts
+++ apps/backend/test/e2e/adoptions.e2e-spec.ts
@@ -8,12 +8,25 @@ import {
 import { MongooseModule } from '@nestjs/mongoose';
 import { AdoptionsModule } from '../../src/adoptions/adoptions.module';
 import { AdoptionSchema } from '../../src/adoptions/adoption.schema';
-import { pet1Mock, pet2Mock, pet3Mock } from '../../test/mocks/adoptions';
+import {
+  countrymen,
+  pet1Mock,
+  pet2Mock,
+  pet3Mock,
+  user1Mock,
+} from '../../test/mocks/adoptions';
 import { AdoptionsService } from '../../src/adoptions/adoptions.service';
+import { AuthModule } from '../../src/auth/auth.module';
+import { AuthService } from '../../src/auth/auth.service';
+import { ConfigModule } from '@nestjs/config';
 
 describe('AdoptionsController (e2e)', () => {
   let app: INestApplication;
-  let service: AdoptionsService;
+  let adoptionsService: AdoptionsService;
+  let authService: AuthService;
+
+  let authToken: string;
+  let userId: string;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -23,13 +36,25 @@ describe('AdoptionsController (e2e)', () => {
           { name: 'Adoption', schema: AdoptionSchema },
         ]),
         AdoptionsModule,
+        AuthModule,
+        ConfigModule.forRoot({ isGlobal: true }),
       ],
     }).compile();
 
     app = moduleFixture.createNestApplication();
-    service = moduleFixture.get<AdoptionsService>(AdoptionsService);
+    adoptionsService = moduleFixture.get<AdoptionsService>(AdoptionsService);
+    authService = moduleFixture.get<AuthService>(AuthService);
 
     await app.init();
+
+    const { id } = await authService.signUp(user1Mock);
+    const { accessToken } = await authService.signIn({
+      email: user1Mock.email,
+      password: user1Mock.password,
+    });
+
+    authToken = accessToken;
+    userId = id.toString();
   });
 
   it('/GET adoptions', async () => {
@@ -46,32 +71,47 @@ describe('AdoptionsController (e2e)', () => {
       observations,
       photos,
       age,
-    } = await service.createAdoption(pet1Mock);
+      type,
+      size,
+    } = await adoptionsService.createAdoption(pet1Mock, 'john@email.com');
     const _id = mongoId.toString();
+
+    const expectedCoordinates = Object.values(user1Mock.location);
 
     return request(app.getHttpServer())
       .get('/api/v1/adoptions')
       .expect(200)
       .expect((res) => {
-        return expect(res.body).toEqual(
-          expect.arrayContaining([
-            expect.objectContaining({
-              _id,
-              name,
-              breed,
-              gender,
-              observations,
-              photos,
-              age,
+        return expect(res.body).toEqual([
+          expect.objectContaining({
+            _id,
+            name,
+            breed,
+            gender,
+            observations,
+            photos,
+            age,
+            type,
+            size,
+            location: {
+              coordinates: expectedCoordinates,
+              type: 'Point',
+            },
+            user: expect.objectContaining({
+              _id: userId,
+              name: 'John',
             }),
-          ]),
-        );
+          }),
+        ]);
       });
   });
 
   it('/POST adoptions', () => {
+    const expectedCoordinates = Object.values(user1Mock.location);
+
     return request(app.getHttpServer())
       .post('/api/v1/adoptions')
+      .auth(authToken, { type: 'bearer' })
       .send(pet2Mock)
       .expect(201)
       .expect((res) =>
@@ -79,13 +119,20 @@ describe('AdoptionsController (e2e)', () => {
           expect.objectContaining({
             ...pet2Mock,
             _id: res.body._id,
+            location: expect.objectContaining({
+              coordinates: expectedCoordinates,
+            }),
+            user: userId,
           }),
         ),
       );
   });
 
   it('/PATCH adoptions', async () => {
-    const { _id: mongoId } = await service.createAdoption(pet2Mock);
+    const { _id: mongoId } = await adoptionsService.createAdoption(
+      pet2Mock,
+      'john@email.com',
+    );
     const id = mongoId.toString();
 
     return request(app.getHttpServer())
@@ -109,7 +156,10 @@ describe('AdoptionsController (e2e)', () => {
       .expect(200)
       .expect([]);
 
-    const expectedPet = await service.createAdoption(pet3Mock);
+    const expectedPet = await adoptionsService.createAdoption(
+      pet3Mock,
+      'john@email.com',
+    );
     const targetId = expectedPet._id.toString();
 
     await request(app.getHttpServer())
@@ -120,6 +170,69 @@ describe('AdoptionsController (e2e)', () => {
       .get('/api/v1/adoptions')
       .expect(200)
       .expect([]);
+  });
+
+  describe('/GET adoptions/nearMe', () => {
+    const [user1, user2] = countrymen;
+
+    beforeEach(async () => {
+      await authService.signUp(user2);
+
+      await adoptionsService.createAdoption(pet2Mock, user2.email);
+      await adoptionsService.createAdoption(pet1Mock, user1.email);
+    });
+
+    it('returns all adoptions within a radius of 1km', async () => {
+      return request(app.getHttpServer())
+        .get('/api/v1/adoptions/nearMe?radius=1')
+        .auth(authToken, { type: 'bearer' })
+        .expect(200)
+        .expect((res) => {
+          return expect(res.body).toEqual([
+            expect.objectContaining({
+              ...pet2Mock,
+              location: expect.objectContaining({
+                coordinates: Object.values(user2.location),
+              }),
+              user: expect.objectContaining({
+                name: user2.name,
+              }),
+            }),
+          ]);
+        });
+    });
+
+    it('does not return adoptions owned by the current user', async () => {
+      return request(app.getHttpServer())
+        .get('/api/v1/adoptions/nearMe?radius=1')
+        .auth(authToken, { type: 'bearer' })
+        .expect(200)
+        .expect((res) => {
+          return expect(res.body).not.toEqual(
+            expect.arrayContaining([
+              expect.objectContaining({
+                ...pet1Mock,
+                location: expect.objectContaining({
+                  coordinates: Object.values(user1.location),
+                }),
+                user: expect.objectContaining({
+                  name: user1.name,
+                }),
+              }),
+            ]),
+          );
+        });
+    });
+
+    it('does not return adoptions out of the radius', async () => {
+      return request(app.getHttpServer())
+        .get('/api/v1/adoptions/nearMe?radius=0.6')
+        .auth(authToken, { type: 'bearer' })
+        .expect(200)
+        .expect((res) => {
+          return expect(res.body).toEqual([]);
+        });
+    });
   });
 
   afterEach(async () => {
--- apps/backend/test/mocks/adoptions.ts
+++ apps/backend/test/mocks/adoptions.ts
@@ -1,4 +1,4 @@
-import { AdoptionType } from '@animavita/models';
+import { AdoptionType, UserType } from '@animavita/models';
 
 export const pet1Mock: AdoptionType = {
   name: 'Bob',
@@ -32,5 +32,27 @@ export const pet3Mock: AdoptionType = {
   photos: [],
   size: 'small',
 };
+
+export const user1Mock: UserType = {
+  email: 'john@email.com',
+  location: {
+    longitude: -47.58849,
+    latitude: -20.90038,
+  },
+  name: 'John',
+  password: 'NicePass123@',
+};
+
+export const user2Mock: UserType = {
+  email: 'paul@email.com',
+  location: {
+    longitude: -47.58839,
+    latitude: -20.9064,
+  },
+  name: 'Paul',
+  password: 'NicePass123@',
+};
+
+export const countrymen = [user1Mock, user2Mock];
 
 export const petsMock = [pet1Mock, pet2Mock, pet3Mock];
