28145a6d5a05f75bf5e105987723946a90d9fc34
Gabriel Belgamo <ggbelgamo@outlook.com>
2023-07-12 19:19:51 +0100

    refactor: move shared stuff to its corresponding folder

--- apps/mobile/src/components/status-bar/status-bar.component.tsx
+++ apps/mobile/src/components/status-bar/status-bar.component.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'native-base';
 
-import theme from '../../../theme';
+import theme from '../../theme';
 
 const AppStatusBar = () => {
   return <StatusBar barStyle="dark-content" backgroundColor={theme.colors.white} />;
