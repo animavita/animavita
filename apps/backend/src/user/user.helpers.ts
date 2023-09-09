9399c8fa52f199171b5211b4081e74260802a51c
Gabriel Belgamo <ggbelgamo@outlook.com>
2023-09-09 09:14:22 +0100

    refactor: rename types

--- apps/backend/src/user/user.helpers.ts
+++ apps/backend/src/user/user.helpers.ts
@@ -1,4 +1,4 @@
 import { UserType } from '@animavita/types';
 
-export const getCoordinatesFromUser = (user: UserDTO) =>
+export const getCoordinatesFromUser = (user: UserType) =>
   Object.values(user.location);
