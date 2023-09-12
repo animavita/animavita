a70b30b29fca790e870eb2659f439fa91a67adb3
Gabriel Belgamo <ggbelgamo@outlook.com>
2023-09-12 15:49:45 +0100

    refactor: apply PR suggestions

--- apps/backend/test/e2e/adoptions.e2e-spec.ts
+++ apps/backend/test/e2e/adoptions.e2e-spec.ts
@@ -21,8 +21,6 @@ import { AuthService } from '../../src/auth/auth.service';
 import { ConfigModule } from '@nestjs/config';
 import { getCoordinatesFromUser } from '../../src/user/user.helpers';
 
-Date.now = () => 1000;
-
 describe('AdoptionsController (e2e)', () => {
   let app: INestApplication;
   let adoptionsService: AdoptionsService;
