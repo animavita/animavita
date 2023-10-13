dac9d0fe185c2e649e1a837de7ef046db8bf3bbc
Isaac Alencar <alencarisaac3@gmail.com>
2023-10-13 10:09:37 -0300

    create a new dto file to represent user creation request

---
+++ shared/types/dtos/auth/sign-up-dto.ts
@@ -0,0 +1,13 @@
+import { Coordinates } from "../user";
+import { CredentialsType } from "./auth-dto";
+
+export type SignUpRequest = {
+  name: string;
+  email: string;
+  password: string;
+  location: Coordinates;
+};
+
+export type SignInResponse = CredentialsType & {
+  name: string;
+};
