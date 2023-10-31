770969d6eb371600252979e8d6d54c336198ed96
purevsurena <a.purevsuren0120@gmail.com>
2023-11-01 00:12:03 +0800

    Configure Babel Alias for Improved Module Resolution Implemented Babel alias configuration to enhance module resolution in the React Native app. This ensures a more dynamic and efficient handling of static imports throughout the codebase.g

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -2,5 +2,15 @@ module.exports = (api) => {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            '@': './src',
+          },
+        },
+      ],
+    ],
   };
 };
