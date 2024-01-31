c2f4c24edce21c3978cd00c4a991e1b9f2c5adc9
Isaac Alencar <alencarisaac3@gmail.com>
2024-01-31 13:58:15 -0300

    replace hard coded copy contents to translations from i18n

--- apps/mobile/src/screens/adoptions/components/header.tsx
+++ apps/mobile/src/screens/adoptions/components/header.tsx
@@ -3,8 +3,11 @@ import { useNavigation } from '@react-navigation/native';
 import { Box, Heading, Icon, Text } from 'native-base';
 import { Pressable } from 'react-native';
 
+import useLocale from '@/hooks/use-locale';
+
 export const Header = () => {
   const { goBack } = useNavigation();
+  const { t } = useLocale();
 
   return (
     <Box mb={4} alignItems="flex-start">
@@ -13,10 +16,10 @@ export const Header = () => {
       </Pressable>
       <Box mt={4}>
         <Heading size="xl" color="primary.500">
-          My Adoptions
+          {t('MY_ADOPTIONS.TITLE')}
         </Heading>
         <Text mt={1} color="coolGray.500">
-          Check adoptions you have created so far!
+          {t('MY_ADOPTIONS.SUB_TITLE')}
         </Text>
       </Box>
     </Box>
