2aad54e8ed47f831cf8e8460177e9674109fc2f1
Gabriel Belgamo <ggbelgamo@outlook.com>
2025-01-01 20:20:33 +0000

    feat: add post pet for adoption use case

---
+++ apps/backend/src/usecases/owner/post-pet-for-adoption.ts
@@ -0,0 +1,38 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { Pet } from '../../domain/pet/pet';
+import PetRepository, { PET_REPOSITORY } from '../../domain/pet/pet.repository';
+import { UserService } from '../../user/user.service';
+import { UserRepository } from '../../user/repositories/user-repository.interface';
+
+type Input = {
+  name: string;
+  breed: string;
+  age: string;
+  type: string;
+  gender: string;
+  size: string;
+  observations: string;
+  photos: string[];
+};
+
+@Injectable()
+export default class PostPetForAdoption {
+  constructor(
+    @Inject(PET_REPOSITORY) private readonly petRepository: PetRepository,
+    @Inject(UserService) private readonly userService: UserRepository,
+  ) {}
+
+  async execute(input: Input, ownerEmail: string) {
+    const owner = await this.userService.findByEmail(ownerEmail);
+
+    const petAttributes = {
+      ...input,
+      ownerId: owner.id,
+      location: owner.location,
+    };
+
+    const pet = Pet.create(petAttributes);
+
+    await this.petRepository.store(pet);
+  }
+}
